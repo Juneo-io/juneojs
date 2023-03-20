6d7f09593a6de7ae5b0ec45b55059a2b40b78d17
Aleksander Waslet <alekswaslet@hotmail.fr>
2023-03-20 18:10:07 +0100

    Added Relay API

---
+++ src/api/relay/api.ts
@@ -0,0 +1,132 @@
+import { AbstractAPI } from '../api'
+import { type JsonRpcResponse, type JuneoClient } from '../client'
+import { type GetBalanceResponse, type GetBlockchainsResponse, type GetBlockchainStatusResponse, type GetBlockResponse, type GetCurrentSupplyResponse, type GetCurrentValidatorsResponse, type GetHeightResponse, type GetMaxStakeAmountResponse, type GetMinStakeResponse, type GetPendingValidatorsResponse, type GetRewardUTXOsResponse, type GetStakeResponse, type GetStakingAssetIDResponse, type GetSupernetsResponse, type GetTimestampResponse, type GetTotalStakeResponse, type GetTxResponse, type GetTxStatusResponse, type GetUTXOsResponse, type GetValidatorsAtResponse, type IssueTxResponse, type SampleValidatorsResponse, type UTXOIndex, type ValidatedByResponse, type ValidatesResponse } from './data'
+
+const MethodCallHeader: string = 'relay'
+const Endpoint = '/ext/bc/Relay'
+
+export class RelayAPI extends AbstractAPI {
+  constructor (client: JuneoClient) {
+    super(client, Endpoint, MethodCallHeader)
+  }
+
+  async getBalance (addresses: string[]): Promise<GetBalanceResponse> {
+    const response: JsonRpcResponse = await this.call('getBalance', [{ addresses }])
+    return response.result
+  }
+
+  async getBlock (blockID: string, encoding?: string): Promise<GetBlockResponse> {
+    const response: JsonRpcResponse = await this.call('getBlock', [{ blockID, encoding }])
+    return response.result
+  }
+
+  async getBlockchains (): Promise<GetBlockchainsResponse> {
+    const response: JsonRpcResponse = await this.call('getBlockchains')
+    return response.result
+  }
+
+  async getBlockchainStatus (blockchainID: string): Promise<GetBlockchainStatusResponse> {
+    const response: JsonRpcResponse = await this.call('getBlockchainStatus', [{ blockchainID }])
+    return response.result
+  }
+
+  async getCurrentSupply (supernetID?: string): Promise<GetCurrentSupplyResponse> {
+    const response: JsonRpcResponse = await this.call('getCurrentSupply', [{ supernetID }])
+    return response.result
+  }
+
+  async getCurrentValidators (supernetID?: string, nodeIDs?: string[]): Promise<GetCurrentValidatorsResponse> {
+    const response: JsonRpcResponse = await this.call('getCurrentValidators', [{ supernetID, nodeIDs }])
+    return response.result
+  }
+
+  async getHeight (): Promise<GetHeightResponse> {
+    const response: JsonRpcResponse = await this.call('getHeight')
+    return response.result
+  }
+
+  async getMaxStakeAmount (supernetID: string, nodeID: string, startTime: number, endTime: number): Promise<GetMaxStakeAmountResponse> {
+    const response: JsonRpcResponse = await this.call('getMaxStakeAmount', [{ supernetID, nodeID, startTime, endTime }])
+    return response.result
+  }
+
+  async getMinStake (supernetID?: string): Promise<GetMinStakeResponse> {
+    const response: JsonRpcResponse = await this.call('getMinStake', [{ supernetID }])
+    return response.result
+  }
+
+  async getPendingValidators (supernetID?: string, nodeIDs?: string[]): Promise<GetPendingValidatorsResponse> {
+    const response: JsonRpcResponse = await this.call('getPendingValidators', [{ supernetID, nodeIDs }])
+    return response.result
+  }
+
+  async getRewardUTXOs (txID: string, encoding?: string): Promise<GetRewardUTXOsResponse> {
+    const response: JsonRpcResponse = await this.call('getRewardUTXOs', [{ txID, encoding }])
+    return response.result
+  }
+
+  async getStake (addresses: string[]): Promise<GetStakeResponse> {
+    const response: JsonRpcResponse = await this.call('getStake', [{ addresses }])
+    return response.result
+  }
+
+  async getStakingAssetID (supernetID?: string): Promise<GetStakingAssetIDResponse> {
+    const response: JsonRpcResponse = await this.call('getStakingAssetID', [{ supernetID }])
+    return response.result
+  }
+
+  async getSupernets (ids: string[]): Promise<GetSupernetsResponse> {
+    const response: JsonRpcResponse = await this.call('getSupernets', [{ ids }])
+    return response.result
+  }
+
+  async getTimestamp (): Promise<GetTimestampResponse> {
+    const response: JsonRpcResponse = await this.call('getTimestamp')
+    return response.result
+  }
+
+  async getTotalStake (supernetID: string): Promise<GetTotalStakeResponse> {
+    const response: JsonRpcResponse = await this.call('getTotalStake', [{ supernetID }])
+    return response.result
+  }
+
+  async getTx (txID: string, encoding?: string): Promise<GetTxResponse> {
+    const response: JsonRpcResponse = await this.call('getTx', [{ txID, encoding }])
+    return response.result
+  }
+
+  async getTxStatus (txID: string): Promise<GetTxStatusResponse> {
+    const response: JsonRpcResponse = await this.call('getTxStatus', [{ txID }])
+    return response.result
+  }
+
+  async getUTXOs (addresses: string[], limit?: number, startIndex?: UTXOIndex, sourceChain?: string, encoding?: string): Promise<GetUTXOsResponse> {
+    const response: JsonRpcResponse = await this.call('getUTXOs', [{ addresses, limit, startIndex, sourceChain, encoding }])
+    return response.result
+  }
+
+  async getValidatorsAt (height: number, supernetID?: string): Promise<GetValidatorsAtResponse> {
+    const response: JsonRpcResponse = await this.call('getValidatorsAt', [{ height, supernetID }])
+    return response.result
+  }
+
+  async issueTx (tx: string, encoding?: string): Promise<IssueTxResponse> {
+    const response: JsonRpcResponse = await this.call('issueTx', [{ tx, encoding }])
+    return response.result
+  }
+
+  async sampleValidators (size: number, supernetID?: string): Promise<SampleValidatorsResponse> {
+    const response: JsonRpcResponse = await this.call('sampleValidators', [{ size, supernetID }])
+    return response.result
+  }
+
+  async validatedBy (blockchainID: string): Promise<ValidatedByResponse> {
+    const response: JsonRpcResponse = await this.call('validatedBy', [{ blockchainID }])
+    return response.result
+  }
+
+  async validates (supernetID: string): Promise<ValidatesResponse> {
+    const response: JsonRpcResponse = await this.call('validates', [{ supernetID }])
+    return response.result
+  }
+}
---
+++ src/api/relay/data.ts
@@ -0,0 +1,196 @@
+
+export interface GetBalanceResponse {
+  balances: Record<string, number>
+  unlockeds: Record<string, number>
+  lockedStakeables: Record<string, number>
+  lockedNotStakeables: Record<string, number>
+  utxoIDs: UtxoID[]
+}
+
+export interface UtxoID {
+  txID: string
+  outputIndex: number
+}
+
+export interface GetBlockResponse {
+  block: string
+  encoding: string
+}
+
+export interface GetBlockchainsResponse {
+  blockchains: Blockchain[]
+}
+
+export interface Blockchain {
+  id: string
+  name: string
+  supernetID: string
+  vmID: string
+  chainAssetID: string
+}
+
+export interface GetBlockchainStatusResponse {
+  status: string
+}
+
+export interface GetCurrentSupplyResponse {
+  supply: number
+}
+
+export interface GetCurrentValidatorsResponse {
+  validators: Validator[]
+}
+
+export interface Validator {
+  txID: string
+  startTime: string
+  endTime: string
+  stakeAmount: string
+  nodeID: string
+  weight: string
+  validationRewardOwner: RewardOwner
+  delegationRewardOwner: RewardOwner
+  potentialReward: string
+  delegationFee: string
+  uptime: string
+  connected: boolean
+  signer: Signer
+  delegatorCount: string
+  delegatorWeight: string
+  delegators: Delegator[]
+}
+
+export interface RewardOwner {
+  locktime: string
+  threshold: string
+  addresses: string[]
+}
+
+export interface Signer {
+  publicKey: string
+  proofOfPossession: string
+}
+
+export interface Delegator {
+  txID: string
+  startTime: string
+  endTime: string
+  stakeAmount: string
+  nodeID: string
+  rewardOwner: RewardOwner
+  potentialReward: string
+}
+
+export interface GetHeightResponse {
+  height: number
+}
+
+export interface GetMaxStakeAmountResponse {
+  amount: number
+}
+
+export interface GetMinStakeResponse {
+  minValidatorStake: number
+  minDelegatorStake: number
+}
+
+export interface GetPendingValidatorsResponse {
+  validators: PendingValidator[]
+  delegators: PendingDelegator[]
+}
+
+export interface PendingValidator {
+  txID: string
+  startTime: string
+  endTime: string
+  stakeAmount: string
+  nodeID: string
+  delegationFee: string
+  connected: boolean
+  signer: Signer
+  weight: string
+}
+
+export interface PendingDelegator {
+  txID: string
+  startTime: string
+  endTime: string
+  stakeAmount: string
+  nodeID: string
+}
+
+export interface GetRewardUTXOsResponse {
+  numFetched: number
+  utxos: string[]
+  encoding: string
+}
+
+export interface GetStakeResponse {
+  stakeds: Record<string, number>
+  stakedOutputs: string[]
+  encoding: string
+}
+
+export interface GetStakingAssetIDResponse {
+  assetID: string
+}
+
+export interface GetSupernetsResponse {
+  supernets: Supernet[]
+}
+
+export interface Supernet {
+  id: string
+  controlKeys: string[]
+  threshold: string
+}
+
+export interface GetTimestampResponse {
+  time: string
+}
+
+export interface GetTotalStakeResponse {
+  stake: number
+  weight: number
+}
+
+export interface GetTxResponse {
+  tx: string
+  encoding: string
+}
+
+export interface GetTxStatusResponse {
+  status: string
+}
+
+export interface GetUTXOsResponse {
+  numFetched: number
+  utxos: string[]
+  endIndex: UTXOIndex
+  encoding: string
+}
+
+export interface UTXOIndex {
+  address: string
+  utxo: string
+}
+
+export interface GetValidatorsAtResponse {
+  validators: Record<string, number>
+}
+
+export interface IssueTxResponse {
+  txID: string
+}
+
+export interface SampleValidatorsResponse {
+  validators: string[]
+}
+
+export interface ValidatedByResponse {
+  supernetID: string
+}
+
+export interface ValidatesResponse {
+  blockchainIDs: string[]
+}

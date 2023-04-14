4e3bc1cd99c2650e426e82d5bc7232a68f2dfef6
Aleksander Waslet <alekswaslet@hotmail.fr>
2023-04-14 13:36:03 +0200

    Added Solidity components

---
+++ src/solidity/abi.ts
@@ -0,0 +1,14 @@
+
+export const BaseERC20ABI: string[] = [
+  'function name() view returns (string)',
+  'function decimals() view returns (uint8)',
+  'function symbol() view returns (string)',
+  'function totalSupply() view returns (uint256)',
+  'function balanceOf(address owner) view returns (uint256)',
+  'function allowance(address owner, address spender) view returns (uint256)',
+  'function transfer(address to, uint amount) returns (bool)',
+  'function approve(address spender, uint256 amount) returns (bool)',
+  'function transferFrom(address from, address to, uint256 amount) returns (bool)',
+  'event Transfer(address indexed from, address indexed to, uint256 value)',
+  'event Approval(address indexed owner, address indexed spender, uint256 value)'
+]

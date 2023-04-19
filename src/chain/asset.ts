daada3e4d018d61727a7c1394204ce724de8ef25
Aleksander Waslet <alekswaslet@hotmail.fr>
2023-04-19 19:20:34 +0200

    Added AssetBalance to handle decimals

--- src/chain/asset.ts
+++ src/chain/asset.ts
@@ -10,3 +10,32 @@ export class JRC20Asset {
     this.contractAddress = contractAddress
   }
 }
+
+export class AssetBalance {
+  value: bigint
+  readonly decimals: number
+
+  constructor (value: bigint, decimals: number) {
+    this.value = value
+    this.decimals = decimals
+  }
+
+  getReadableValue (): string {
+    const stringValue: string = this.value.toString()
+    const length: number = stringValue.length
+    if (length <= this.decimals) {
+      return `0.${stringValue.padStart(this.decimals, '0')}`
+    } else {
+      let readableValue: string = stringValue.substring(0, length - this.decimals)
+      readableValue += '.'
+      readableValue += stringValue.substring(length - this.decimals, length)
+      return readableValue
+    }
+  }
+
+  getReadableValueRounded (decimals: number = 2): string {
+    const readableValue: string = this.getReadableValue()
+    const index: number = readableValue.indexOf('.')
+    return readableValue.substring(0, index + decimals + 1)
+  }
+}

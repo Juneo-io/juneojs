49548427d99a69fb2974592c6f0d4744606900ce
Aleksander Waslet <alekswaslet@hotmail.fr>
2023-08-20 14:46:01 +0200

    Sorted examples

--- examples/docs/addValidator.ts
+++ examples/docs/addValidator.ts
@@ -1,22 +1,24 @@
 import { MCNProvider, JuneoWallet, StakeManager, now } from '../../src'
 
 async function main () {
+    // provider to interact with the MCN
     const provider: MCNProvider = new MCNProvider()
+    // recovering wallet used to sign transactions
     const wallet: JuneoWallet = JuneoWallet.recover('raven whip pave toy benefit moment twin acid wasp satisfy crash april')
     // stake manager to handle staking
     const manager: StakeManager = new StakeManager(provider, wallet)
-    // the node id where to delegate funds
+
+    // the node id which will validate the funds
     const nodeId: string = 'NodeID-DXGCAZFrcwfBmgXMePrTm2EU8N3s46wEq'
-    // the amount to delegate
-    const stakeAmount: bigint = BigInt(1000000000)
+    // the amount to validate
+    const stakeAmount: bigint = BigInt(100 * (10**9)) // 100 JUNE
     // the time to start delegation (must be > now)
     const startTime: bigint = now() + BigInt(30)
-    // the time to end the delegation with start time is staking period
+    // the time to end the validation with start time is staking period
     // staking period has a minimal and maximal value
     const endTime: bigint = now() + BigInt(86400 * 15)
-    // try to delegate with currently available utxos in the relay chain
-    manager.delegate(nodeId, stakeAmount, startTime, endTime)
-    // if you want to validate instead of delegate use manager.validate instead
+    // try to validate with currently available utxos in the relay chain
+    manager.validate(nodeId, stakeAmount, startTime, endTime)
 }
 
 main().catch((error) => {

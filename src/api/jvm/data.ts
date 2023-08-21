a53d9e7d27a8be05d2f435c2d662cd95cbba8920
Aleksander Waslet <alekswaslet@hotmail.fr>
2023-08-21 15:50:21 +0200

    Updated APIs functions

--- src/api/jvm/data.ts
+++ src/api/jvm/data.ts
@@ -4,39 +4,9 @@ export interface BuildGenesisResponse {
   encoding: string
 }
 
-/**
- * @deprecated
- */
-export interface GetAddressTxsResponse {
-  txIDS: string[]
-  cursor: number
-}
-
-/**
- * @deprecated
- */
-export interface GetAllBalancesResponse {
-  balances: Balance[]
-}
-
-/**
- * @deprecated
- */
-export interface Balance {
-  asset: string
-  balance: number
-}
-
 export interface GetAssetDescriptionResponse {
   assetID: string
   name: string
   symbol: string
   denomination: number
-}
-
-/**
- * @deprecated
- */
-export interface GetJVMBalanceResponse {
-  balance: number
 }

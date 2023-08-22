ed0107a272a4f6297322bcdd6cbb167aa42f1786
Aleksander Waslet <alekswaslet@hotmail.fr>
2023-08-22 20:26:15 +0200

    Added estimateSend

--- src/wallet/send.ts
+++ src/wallet/send.ts
@@ -1,10 +1,10 @@
 import { type ethers } from 'ethers'
 import { type JEVMAPI, type JVMAPI } from '../api'
-import { JEVMBlockchain } from '../chain'
+import { type Blockchain, JEVMBlockchain } from '../chain'
 import { type JuneoWallet, type VMWallet } from './wallet'
 import { EVMTransactionData, estimateEVMTransaction, sendEVMTransaction } from './common'
 import { type EVMFeeData } from './fee'
-import { FeeType, UserInput, type Utxo, buildJVMBaseTransaction, fetchUtxos } from '../transaction'
+import { FeeType, UserInput, type Utxo, buildJVMBaseTransaction, fetchUtxos, FeeData } from '../transaction'
 import { type MCNProvider } from '../juneo'
 
 export class SendManager {
@@ -16,24 +16,44 @@ export class SendManager {
     this.wallet = wallet
   }
 
-  async sendEVM (chainId: string, assetId: string, amount: bigint, address: string): Promise<string> {
+  async estimateSendEVM (chainId: string, assetId: string, amount: bigint, address: string): Promise<EVMFeeData> {
     const api: JEVMAPI = this.provider.jevm[chainId]
     const wallet: ethers.Wallet = this.wallet.getEthWallet(api.chain).evmWallet
     const isContract: boolean = JEVMBlockchain.isContractAddress(assetId)
     const to: string = isContract ? assetId : address
     const value: bigint = isContract ? BigInt(0) : amount
     const data: string = isContract ? await api.chain.getContractTransactionData(assetId, address, amount) : '0x'
-    const feeData: EVMFeeData = await estimateEVMTransaction(api, wallet.address, to, value, data, FeeType.BaseFee)
+    return await estimateEVMTransaction(api, wallet.address, to, value, data, FeeType.BaseFee)
+  }
+
+  async sendEVM (chainId: string, assetId: string, amount: bigint, address: string, feeData?: EVMFeeData): Promise<string> {
+    const api: JEVMAPI = this.provider.jevm[chainId]
+    const wallet: ethers.Wallet = this.wallet.getEthWallet(api.chain).evmWallet
+    const isContract: boolean = JEVMBlockchain.isContractAddress(assetId)
+    const to: string = isContract ? assetId : address
+    const value: bigint = isContract ? BigInt(0) : amount
+    const data: string = isContract ? await api.chain.getContractTransactionData(assetId, address, amount) : '0x'
+    if (typeof feeData === 'undefined') {
+      feeData = await estimateEVMTransaction(api, wallet.address, to, value, data, FeeType.BaseFee)
+    }
     return await sendEVMTransaction(api, wallet, new EVMTransactionData(to, value, feeData, data))
   }
 
-  async sendJVM (assetId: string, amount: bigint, address: string): Promise<string> {
+  async estimateSendJVM (): Promise<FeeData> {
+    const fee: bigint = BigInt((await this.provider.info.getTxFee()).txFee)
+    const chain: Blockchain = this.provider.jvm.chain
+    return new FeeData(chain, fee, chain.assetId, FeeType.BaseFee)
+  }
+
+  async sendJVM (assetId: string, amount: bigint, address: string, feeData?: FeeData): Promise<string> {
     const api: JVMAPI = this.provider.jvm
     const wallet: VMWallet = this.wallet.getWallet(api.chain)
     const utxoSet: Utxo[] = await fetchUtxos(api, [wallet.getAddress()])
-    const fee: bigint = await api.chain.queryBaseFee(this.provider)
+    if (typeof feeData === 'undefined') {
+      feeData = await this.estimateSendJVM()
+    }
     const transaction: string = buildJVMBaseTransaction([new UserInput(assetId, api.chain, amount, address, api.chain)],
-      utxoSet, [wallet.getAddress()], fee, wallet.getAddress(), this.provider.mcn.id, api.chain.id
+      utxoSet, [wallet.getAddress()], feeData.amount, wallet.getAddress(), this.provider.mcn.id, api.chain.id
     ).signTransaction([wallet]).toCHex()
     return (await api.issueTx(transaction)).txID
   }

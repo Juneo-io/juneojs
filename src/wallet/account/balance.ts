42d50a3ba97e69af1d85a02b49f970e430ba41c2
Aleksander Waslet <alekswaslet@hotmail.fr>
2023-08-30 13:37:33 +0200

    Added BalanceListener

--- src/wallet/account/balance.ts
+++ src/wallet/account/balance.ts
@@ -5,15 +5,36 @@ export enum BalanceStatus {
   Done = 'Done',
 }
 
+export class BalanceUpdateEvent {
+  previousValue: bigint
+  value: bigint = BigInt(0)
+
+  constructor (previousValue: bigint) {
+    this.previousValue = previousValue
+  }
+}
+
+export interface BalanceListener {
+  onBalanceUpdateEvent: (event: BalanceUpdateEvent) => void
+}
+
 export class Balance {
+  private readonly listeners: BalanceListener[] = []
   private status: BalanceStatus = BalanceStatus.Initializing
   private value: bigint = BigInt(0)
+
+  registerEvents (listener: BalanceListener): void {
+    this.listeners.push(listener)
+  }
 
   update (value: bigint): void {
     if (this.status === BalanceStatus.Updating) {
       return
     }
+    const event: BalanceUpdateEvent = new BalanceUpdateEvent(this.value)
     this.value = value
+    event.value = value
+    this.callBalanceUpdateEvent(event)
   }
 
   async updateAsync (fetch: Promise<bigint>): Promise<void> {
@@ -21,8 +42,11 @@ export class Balance {
       return
     }
     this.status = BalanceStatus.Updating
+    const event: BalanceUpdateEvent = new BalanceUpdateEvent(this.value)
     this.value = await fetch
     this.status = BalanceStatus.Done
+    event.value = this.value
+    this.callBalanceUpdateEvent(event)
   }
 
   getStatus (): BalanceStatus {
@@ -34,6 +58,15 @@ export class Balance {
   }
 
   spend (value: bigint): void {
+    const event: BalanceUpdateEvent = new BalanceUpdateEvent(this.value)
     this.value -= value
+    event.value = this.value
+    this.callBalanceUpdateEvent(event)
+  }
+
+  private callBalanceUpdateEvent (event: BalanceUpdateEvent): void {
+    this.listeners.forEach(listener => {
+      listener.onBalanceUpdateEvent(event)
+    })
   }
 }

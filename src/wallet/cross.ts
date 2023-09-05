7459a285694bd2f0701a238cac1e9c535e52e145
Aleksander Waslet <alekswaslet@hotmail.fr>
2023-09-05 17:49:30 +0200

    EVM cross fixes and added proxy cross WIP

--- src/wallet/cross.ts
+++ src/wallet/cross.ts
@@ -1,14 +1,14 @@
 import { type AbstractUtxoAPI, type JEVMAPI } from '../api'
-import { JVM_ID, PLATFORMVM_ID, JEVM_ID, type Blockchain } from '../chain'
+import { JVM_ID, PLATFORMVM_ID, JEVM_ID, type Blockchain, SocotraJUNEChain, JEVMBlockchain } from '../chain'
 import { type MCNProvider } from '../juneo'
 import { fetchUtxos, type Utxo } from '../transaction'
 import { CrossError } from '../utils'
 import { type ChainAccount, type MCNAccount, type UtxoAccount } from './account'
-import { type MCNOperation, MCNOperationType, type ExecutableMCNOperation, type MCNOperationSummary } from './operation'
+import { type MCNOperation, MCNOperationType, type ExecutableMCNOperation, MCNOperationSummary } from './operation'
 import {
   estimateEVMExportTransaction, estimateEVMImportTransaction, estimateJVMExportTransaction, estimateJVMImportTransaction,
   estimatePlatformExportTransaction, estimatePlatformImportTransaction, sendJVMExportTransaction, type FeeData,
-  sendPlatformExportTransaction, sendJVMImportTransaction, sendPlatformImportTransaction, sendEVMImportTransaction, sendEVMExportTransaction, type BaseFeeData, TransactionType
+  sendPlatformExportTransaction, sendJVMImportTransaction, sendPlatformImportTransaction, sendEVMImportTransaction, sendEVMExportTransaction, type BaseFeeData, TransactionType, type Spending, BaseSpending
 } from './transaction'
 import { type JuneoWallet } from './wallet'
 
@@ -58,6 +58,18 @@ export class CrossManager {
     return sendImportFee
   }
 
+  shouldProxy (cross: CrossOperation): boolean {
+    // with current logic we do not want june funds to be in other EVMs
+    // that puts a constraint that if we want to export funds from another EVM to the JUNE chain we in fact
+    // do not have june in it to be able to export the fee. Added to the contraint of EVM that are using nonce
+    // accounts and not utxos account, it is not possible to pay the import fee with the destination balance.
+    // to solve that problem, we use the JVM to do a proxy in this transaction. JVM having utxos accounts it is
+    // so possible to export only of EVM's chain asset and then pay the import fee in the JVM in june. Then the
+    // initial cross transaction can be finalized by doing a regular JVM to JUNE chain cross transaction with the
+    // initial exported funds towards it.
+    return cross.source.vmId === JEVM_ID && cross.destination.vmId === JEVM_ID && cross.source.id !== SocotraJUNEChain.id
+  }
+
   async export (
     source: Blockchain, destination: Blockchain, assetId: string, amount: bigint, address: string,
     sendImportFee: boolean = true, importFee?: FeeData, exportFee?: FeeData, utxoSet?: Utxo[]
@@ -89,6 +101,10 @@ export class CrossManager {
     if (source.id === destination.id) {
       throw new CrossError('source and destination chain cannot be the same')
     }
+    // jevm cross transactions amount must be changed because of atomic denominator
+    if (source.vmId === JEVM_ID && assetId === source.assetId) {
+      amount /= JEVMBlockchain.AtomicDenomination
+    }
     if (destination.vmId === JVM_ID) {
       return await sendJVMImportTransaction(this.provider, this.wallet, source, assetId, amount, address, payImportFee, importFee, utxoSet)
     } else if (destination.vmId === PLATFORMVM_ID) {
@@ -103,6 +119,50 @@ export class CrossManager {
     throw new CrossError(`destination vm id does not support cross: ${destination.vmId}`)
   }
 
+  async estimateCrossOperation (cross: CrossOperation, account: MCNAccount): Promise<MCNOperationSummary> {
+    // WIP START <--------->
+    if (this.shouldProxy(cross)) {
+      const chains: Blockchain[] = []
+      const fees: FeeData[] = []
+      const spendings: Spending[] = []
+      const proxyExport: CrossOperation = new CrossOperation(cross.source, this.provider.jvm.chain, cross.assetId, cross.amount, cross.address)
+      const exportSummary: MCNOperationSummary = await this.estimateCrossOperation(proxyExport, account)
+      chains.push(...exportSummary.chains)
+      fees.push(...exportSummary.fees)
+      spendings.push(...exportSummary.spendings)
+      const proxyImport: CrossOperation = new CrossOperation(this.provider.jvm.chain, cross.destination, cross.assetId, cross.amount, cross.address)
+      const importSummary: MCNOperationSummary = await this.estimateCrossOperation(proxyImport, account)
+      chains.push(...importSummary.chains)
+      fees.push(...importSummary.fees)
+      spendings.push(...importSummary.spendings)
+      return new MCNOperationSummary(cross, chains, fees, spendings)
+    }
+    // WIP END <--------->
+    const chains: Blockchain[] = [cross.source, cross.destination]
+    const exportFee: BaseFeeData = await this.estimateExport(cross.source, cross.destination, cross.assetId)
+    const importFee: BaseFeeData = await this.estimateImport(cross.destination, cross.assetId)
+    const fees: BaseFeeData[] = [exportFee, importFee]
+    const sourceAccount: ChainAccount = account.getAccount(cross.source.id)
+    const destinationAccount: ChainAccount = account.getAccount(cross.destination.id)
+    const destinationBalance: bigint = destinationAccount.getValue(importFee.assetId)
+    let sourceBalance: bigint = sourceAccount.getValue(importFee.assetId)
+    if (exportFee.assetId === importFee.assetId) {
+      sourceBalance -= exportFee.amount
+    }
+    if (cross.assetId === importFee.assetId) {
+      sourceBalance -= cross.amount
+    }
+    const sendImportFee: boolean = this.shouldSendImportFee(cross.destination, importFee.amount, destinationBalance, sourceBalance)
+    cross.sendImportFee = sendImportFee
+    const spendings: Spending[] = [new BaseSpending(cross.source.id, cross.amount, cross.assetId), exportFee]
+    if (sendImportFee) {
+      spendings.push(new BaseSpending(cross.source.id, importFee.amount, importFee.assetId))
+    } else {
+      spendings.push(importFee)
+    }
+    return new MCNOperationSummary(cross, chains, fees, spendings)
+  }
+
   async executeCrossOperation (executable: ExecutableMCNOperation, account: MCNAccount): Promise<void> {
     const summary: MCNOperationSummary = executable.summary
     const operation: MCNOperation = summary.operation
@@ -110,6 +170,12 @@ export class CrossManager {
       throw new CrossError(`operation ${operation.type} is forbidden`)
     }
     const cross: CrossOperation = operation as CrossOperation
+    // WIP START <--------->
+    if (this.shouldProxy(cross)) {
+      // const proxyExport: CrossOperation = new CrossOperation(cross.source, this.provider.jvm.chain, cross.assetId, cross.amount, cross.address)
+      // await this.executeCrossOperation(proxyExport, account)
+    }
+    // WIP END <--------->
     const exportFee: FeeData = executable.summary.fees[0]
     const importFee: FeeData = executable.summary.fees[1]
     let sourceUtxos: Utxo[] = []
@@ -153,7 +219,7 @@ export class CrossManager {
       destinationUtxos.push(...(sourceAccount as UtxoAccount).utxoSet)
     }
     const importTransactionId: string = await this.import(
-      cross.source, cross.destination, cross.assetId, cross.amount, cross.address, cross.sendImportFee, importFee, destinationUtxos
+      cross.source, cross.destination, cross.assetId, cross.amount, cross.address, !cross.sendImportFee, importFee, destinationUtxos
     )
     let importSuccess: boolean = false
     if (destinationVmId === JVM_ID) {
@@ -161,7 +227,7 @@ export class CrossManager {
     } else if (destinationVmId === PLATFORMVM_ID) {
       importSuccess = await executable.addTrackedPlatformTransaction(this.provider.platform, TransactionType.Import, importTransactionId)
     } else if (destinationVmId === JEVM_ID) {
-      const api: JEVMAPI = this.provider.jevm[sourceAccount.chain.id]
+      const api: JEVMAPI = this.provider.jevm[destinationAccount.chain.id]
       importSuccess = await executable.addTrackedJEVMTransaction(api, TransactionType.Import, importTransactionId)
     }
     await destinationAccount.fetchAllBalances()

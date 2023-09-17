ef9b5ea8add3bd3b0ae995f1f7ae795dbaadbcfb
Aleksander Waslet <alekswaslet@hotmail.fr>
2023-09-17 19:25:04 +0200

    Added signature recovery param

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -1,6 +1,6 @@
 import { Buffer } from 'buffer/'
 import hash from 'create-hash'
-import { getPublicKey, signSync, Signature } from '@noble/secp256k1'
+import { Signature, recoverPublicKey, getPublicKey, signSync } from '@noble/secp256k1'
 import { JuneoBuffer } from './bytes'
 
 export const SignatureLength: number = 65
@@ -19,6 +19,10 @@ export function sha256 (data: string | JuneoBuffer): JuneoBuffer {
   return JuneoBuffer.fromBytes(Buffer.from(hash('sha256').update(buffer.getBytes()).digest()))
 }
 
+function recoverPubKey (hash: Buffer, signature: Signature, recovery: number): string {
+  return JuneoBuffer.fromBytes(Buffer.from(recoverPublicKey(hash, signature, recovery, true))).toHex().padStart(66, '0')
+}
+
 export class ECKeyPair {
   private readonly privateKey: string
   readonly publicKey: string
@@ -30,6 +34,8 @@ export class ECKeyPair {
 
   sign (buffer: JuneoBuffer): JuneoBuffer {
     const signature: Signature = Signature.fromHex(signSync(buffer.getBytes(), this.privateKey))
-    return JuneoBuffer.fromString(signature.toCompactHex())
+    // noble as of v1.7.1 does not provide recovery param so do it here
+    const v: number = recoverPubKey(buffer.getBytes(), signature, 0) === this.publicKey ? 0 : 1
+    return JuneoBuffer.fromString(`${signature.toCompactHex()}${v.toString(16).padStart(2, '0')}`)
   }
 }

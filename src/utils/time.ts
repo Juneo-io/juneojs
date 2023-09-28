6ad7c158d98c7aefab3f786a3664e592119d4362
alekswaslet <1346150+alekswaslet@users.noreply.github.com>
2023-09-28 16:15:35 +0200

    Refactored staking pre-checks

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,8 +1,23 @@
-// TODO get time from the platform to avoid using local machine timestamp
+import { TimeError } from './errors'
+
+// TODO get time from an external service to avoid using local machine timestamp
 export function now (): bigint {
   return BigInt(Math.round(new Date().getTime() / 1000))
 }
 
 export async function sleep (milliseconds: number): Promise<void> {
   await new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
+export function verifyTimeRange (startTime: bigint, endTime: bigint, minPeriod: bigint): void {
+  if (startTime < now()) {
+    throw new TimeError('start time must be in the future')
+  }
+  if (endTime <= startTime) {
+    throw new TimeError('end time must be after start time')
+  }
+  const period: bigint = endTime - startTime
+  if (period < minPeriod) {
+    throw new TimeError(`period must be at least ${minPeriod} seconds`)
+  }
 }
--- src/wallet/stake.ts
+++ src/wallet/stake.ts
@@ -2,9 +2,9 @@ import { NetworkOperationType, ChainNetworkOperation } from './operation'
 import { NodeId, type Utxo, Validator } from '../transaction'
 import { type UtxoFeeData, estimatePlatformAddValidatorTransaction, estimatePlatformAddDelegatorTransaction } from './transaction'
 import { type MCNWallet, type VMWallet } from './wallet'
-import { WalletError, calculatePrimary, now } from '../utils'
+import { StakeError, calculatePrimary, now, verifyTimeRange } from '../utils'
 import { type PlatformAPI } from '../api'
-import { type MCN } from '../chain'
+import { type StakeConfig, type MCN } from '../chain'
 import { type MCNProvider } from '../juneo'
 
 export const ValidationShare: number = 12_0000 // 12%
@@ -35,6 +35,33 @@ export class StakeManager {
     return rewards * BigInt(DelegationShare) / BigInt(BaseShare)
   }
 
+  static verifyStakingValues (
+    amount: bigint, minStake: bigint, maxStake: bigint, startTime: bigint, endTime: bigint, minPeriod: bigint
+  ): void {
+    if (amount < minStake) {
+      throw new StakeError(`amount ${amount} is less than min stake ${minStake}`)
+    }
+    if (amount > maxStake) {
+      throw new StakeError(`amount ${amount} exceeds max stake ${maxStake}`)
+    }
+    verifyTimeRange(startTime, endTime, minPeriod)
+  }
+
+  static verifyValidationValues (amount: bigint, startTime: bigint, endTime: bigint, config: StakeConfig): void {
+    const minStake: bigint = config.minValidatorStake
+    const maxStake: bigint = config.maxValidatorStake
+    const minPeriod: bigint = config.minStakeDuration
+    StakeManager.verifyStakingValues(amount, minStake, maxStake, startTime, endTime, minPeriod)
+  }
+
+  static verifyDelegationValues (amount: bigint, startTime: bigint, endTime: bigint, config: StakeConfig): void {
+    const minStake: bigint = config.minDelegatorStake
+    // this is not a correct max value but can be used for a sanity pre-check
+    const maxStake: bigint = config.maxValidatorStake
+    const minPeriod: bigint = config.minStakeDuration
+    StakeManager.verifyStakingValues(amount, minStake, maxStake, startTime, endTime, minPeriod)
+  }
+
   async estimateValidationFee (nodeId: string, amount: bigint, startTime: bigint, endTime: bigint, utxoSet?: Utxo[]): Promise<UtxoFeeData> {
     const validator: Validator = new Validator(new NodeId(nodeId), startTime, endTime, amount)
     return await estimatePlatformAddValidatorTransaction(this.provider, this.wallet, validator, ValidationShare, utxoSet)
@@ -45,42 +72,20 @@ export class StakeManager {
     return await estimatePlatformAddDelegatorTransaction(this.provider, this.wallet, validator, utxoSet)
   }
 
-  private checkTimeValidity (startTime: bigint, endTime: bigint): void {
-    if (startTime < now()) {
-      throw new WalletError('Stake start time must be in the future')
-    }
-    if (endTime <= startTime) {
-      throw new WalletError('Stake end time must be after start time')
-    }
-  }
-
   async validate (nodeId: string, amount: bigint, startTime: bigint, endTime: bigint, feeData?: UtxoFeeData, utxoSet?: Utxo[]): Promise<string> {
+    StakeManager.verifyValidationValues(amount, startTime, endTime, this.provider.mcn.stakeConfig)
     if (typeof feeData === 'undefined') {
       feeData = await this.estimateValidationFee(nodeId, amount, startTime, endTime, utxoSet)
-    }
-    // check if the time is valid
-    this.checkTimeValidity(startTime, endTime)
-    if (endTime - startTime < this.provider.mcn.stakeConfig.minStakeDuration) {
-      throw new WalletError(`Stake duration must be at least ${this.provider.mcn.stakeConfig.minStakeDuration} seconds`)
-    }
-    const minStake: bigint = this.provider.mcn.stakeConfig.minValidatorStake
-    const maxStake: bigint = this.provider.mcn.stakeConfig.maxValidatorStake
-    if (amount > maxStake) {
-      throw new WalletError(`Stake amount ${amount} exceeds max stake ${maxStake}`)
-    }
-    if (amount < minStake) {
-      throw new WalletError(`Stake amount ${amount} is less than min stake ${minStake}`)
     }
     const transaction: string = feeData.transaction.signTransaction([this.wallet]).toCHex()
     return (await this.api.issueTx(transaction)).txID
   }
 
   async delegate (nodeId: string, amount: bigint, startTime: bigint, endTime: bigint, feeData?: UtxoFeeData, utxoSet?: Utxo[]): Promise<string> {
+    StakeManager.verifyDelegationValues(amount, startTime, endTime, this.provider.mcn.stakeConfig)
     if (typeof feeData === 'undefined') {
       feeData = await this.estimateValidationFee(nodeId, amount, startTime, endTime, utxoSet)
     }
-    // check if node already validate (must be true)
-    this.checkTimeValidity(startTime, endTime)
     const transaction: string = feeData.transaction.signTransaction([this.wallet]).toCHex()
     return (await this.api.issueTx(transaction)).txID
   }

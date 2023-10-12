d0d33b0c3b4383bd4cecf51f0ac5573d668f9cf6
alekswaslet <1346150+alekswaslet@users.noreply.github.com>
2023-10-12 17:59:54 +0200

    Updated cross tests

--- tests/e2e/cross.test.ts
+++ tests/e2e/cross.test.ts
@@ -1,4 +1,3 @@
-import * as dotenv from 'dotenv'
 import {
   AccountError,
   CrossOperation,
@@ -6,85 +5,260 @@ import {
   MCNAccount,
   MCNProvider,
   MCNWallet,
-  SocotraEUROC1AssetId,
   SocotraEUROC1Chain,
-  SocotraJUNEAssetId,
   SocotraJUNEChain,
   SocotraJVMChain,
   SocotraPlatformChain,
-  type ExecutableOperation
+  type ExecutableOperation,
+  SocotraNetwork,
+  NetworkOperationRange,
+  NetworkOperationType
 } from '../../src'
+import * as dotenv from 'dotenv'
 dotenv.config()
 
-describe('Cross Operations', () => {
+describe('Cross operations', () => {
   const wallet = MCNWallet.recover(process.env.MNEMONIC ?? '')
-  const provider: MCNProvider = new MCNProvider()
+  const provider: MCNProvider = new MCNProvider(SocotraNetwork)
   const mcnAccount: MCNAccount = new MCNAccount(provider, wallet)
-
   const EXCESSIVE_AMOUNT = BigInt('100000000000000000000000000000000000000000000000')
-
-  beforeAll(async () => {
-    mcnAccount.getAccount(SocotraEUROC1Chain.id)
-    mcnAccount.getAccount(SocotraJVMChain.id)
-    mcnAccount.getAccount(SocotraPlatformChain.id)
-  })
+  const DEFAULT_TIMEOUT: number = 10_000
+  const DONE_STATUS = 'Done'
+  const juneChain = SocotraJUNEChain
+  const euroChain = SocotraEUROC1Chain
+  const platformChain = SocotraPlatformChain
+  const jvmChain = SocotraJVMChain
 
   beforeEach(async () => {
     await mcnAccount.fetchChainsBalances()
   })
 
-  describe('Instanciation', () => {
+  describe('Instantiation', () => {
     test.each([
-      ['JUNE-Chain to EUROC1-Chain', SocotraJUNEChain, SocotraEUROC1Chain, '0x3300000000000000000000000000000000000000', BigInt(1000)],
-      ['EUROC1-Chain to JUNE-Chain', SocotraEUROC1Chain, SocotraJUNEChain, SocotraEUROC1AssetId, BigInt(10000000000000)],
-      ['JUNE-Chain to JVM-Chain', SocotraJUNEChain, SocotraJVMChain, SocotraJUNEAssetId, BigInt(100000000000)],
-      ['JUNE-Chain to EUROC1-Chain with different assetId', SocotraJUNEChain, SocotraEUROC1Chain, SocotraEUROC1AssetId, BigInt(5000)],
-      ['JUNE-Chain to Platform-Chain', SocotraJUNEChain, SocotraPlatformChain, '0x4400000000000000000000000000000000000000', BigInt(6000)],
-      ['JUNE-Chain to EUROC1-Chain with excessive amount', SocotraJUNEChain, SocotraEUROC1Chain, '0x3300000000000000000000000000000000000000', EXCESSIVE_AMOUNT],
-      ['JUNE-Chain to EUROC1-Chain with zero amount', SocotraJUNEChain, SocotraEUROC1Chain, '0x3300000000000000000000000000000000000000', BigInt(0)],
-      ['JUNE-Chain to EUROC1-Chain with negative amount', SocotraJUNEChain, SocotraEUROC1Chain, '0x3300000000000000000000000000000000000000', BigInt(-1000)]
-    ])('%s', async (description, src, dest, assetId, value) => {
-      const operation = new CrossOperation(src, dest, assetId, value)
-      expect(operation.source).toEqual(src)
-      expect(operation.assetId).toEqual(assetId)
-    })
+      {
+        source: juneChain,
+        destination: euroChain,
+        assetId: '0x3300000000000000000000000000000000000000',
+        symbol: 'EUROC.e',
+        value: BigInt(1_000)
+      },
+      {
+        source: euroChain,
+        destination: juneChain,
+        assetId: euroChain.assetId,
+        symbol: euroChain.asset.symbol,
+        value: BigInt(10_000_000_000_000)
+      },
+      {
+        source: juneChain,
+        destination: jvmChain,
+        assetId: juneChain.assetId,
+        symbol: juneChain.asset.symbol,
+        value: BigInt(100_000_000_000)
+      },
+      {
+        source: juneChain,
+        destination: euroChain,
+        assetId: euroChain.assetId,
+        symbol: euroChain.asset.symbol,
+        value: BigInt(5_000)
+      },
+      {
+        source: juneChain,
+        destination: platformChain,
+        assetId: '0x4400000000000000000000000000000000000000',
+        symbol: 'UNDEFINED',
+        value: BigInt(6_000)
+      },
+      {
+        source: juneChain,
+        destination: euroChain,
+        assetId: '0x3300000000000000000000000000000000000000',
+        symbol: 'EUROC.e',
+        value: EXCESSIVE_AMOUNT
+      },
+      {
+        source: juneChain,
+        destination: euroChain,
+        assetId: '0x3300000000000000000000000000000000000000',
+        symbol: 'EUROC.e',
+        value: BigInt(0)
+      },
+      {
+        source: juneChain,
+        destination: euroChain,
+        assetId: '0x3300000000000000000000000000000000000000',
+        symbol: 'EUROC.e',
+        value: BigInt(-1_000)
+      }
+    ])(
+      '$#) $value $symbol from $source.name to $destination.name',
+      async ({ source, destination, assetId, value }) => {
+        const operation = new CrossOperation(source, destination, assetId, value)
+        expect(operation.amount).toEqual(value)
+        expect(operation.assetId).toEqual(assetId)
+        expect(operation.destination).toEqual(destination)
+        expect(operation.source).toEqual(source)
+        expect(operation.range).toEqual(NetworkOperationRange.Supernet)
+        expect(operation.type).toEqual(NetworkOperationType.Cross)
+      },
+      DEFAULT_TIMEOUT
+    )
   })
 
-  describe('Valid CrossOperation', () => {
+  describe('Valid execute', () => {
     test.each([
-      ['JUNE-Chain to EUROC1-Chain', SocotraJUNEChain, SocotraEUROC1Chain, '0x3300000000000000000000000000000000000000', BigInt(1000)],
-      ['EUROC1-Chain to JUNE-Chain', SocotraEUROC1Chain, SocotraJUNEChain, SocotraEUROC1AssetId, BigInt(10000000000000)],
-      ['JUNE-Chain to JVM-Chain', SocotraJUNEChain, SocotraJVMChain, SocotraJUNEAssetId, BigInt(100000000000)],
-      ['JUNE-Chain to Platform-Chain', SocotraJUNEChain, SocotraPlatformChain, SocotraJUNEAssetId, BigInt(1000000000000)],
-      ['Platform-Chain to JUNE-Chain', SocotraPlatformChain, SocotraJUNEChain, SocotraJUNEAssetId, BigInt(1000000)],
-      ['Platform-Chain to JVM-Chain', SocotraPlatformChain, SocotraJVMChain, SocotraJUNEAssetId, BigInt(1000000)],
-      ['JVM-Chain to Platform-Chain', SocotraJVMChain, SocotraPlatformChain, SocotraJUNEAssetId, BigInt(1000000)],
-      ['JVM-Chain to JUNE-Chain', SocotraJVMChain, SocotraJUNEChain, SocotraJUNEAssetId, BigInt(1000000)]
-    ])('%s', async (description, src, dest, assetId, value) => {
-      const operation = new CrossOperation(src, dest, assetId, value)
-      const summary = await mcnAccount.estimate(operation)
-      const executable: ExecutableOperation = summary.getExecutable()
-
-      await mcnAccount.execute(summary)
-      expect(executable.status).toEqual('Done')
-    }, 15000)
+      {
+        source: juneChain,
+        destination: euroChain,
+        assetId: '0x3300000000000000000000000000000000000000',
+        symbol: 'EUROC.e',
+        value: BigInt(1_000)
+      },
+      {
+        source: euroChain,
+        destination: juneChain,
+        assetId: euroChain.assetId,
+        symbol: euroChain.asset.symbol,
+        value: BigInt(10_000_000_000_000)
+      },
+      {
+        source: juneChain,
+        destination: jvmChain,
+        assetId: juneChain.assetId,
+        symbol: juneChain.asset.symbol,
+        value: BigInt(100_000_000_000)
+      },
+      {
+        source: juneChain,
+        destination: platformChain,
+        assetId: juneChain.assetId,
+        symbol: juneChain.asset.symbol,
+        value: BigInt(1_000_000_000_000)
+      },
+      {
+        source: platformChain,
+        destination: juneChain,
+        assetId: platformChain.assetId,
+        symbol: platformChain.asset.symbol,
+        value: BigInt(1_000_000)
+      },
+      {
+        source: platformChain,
+        destination: jvmChain,
+        assetId: platformChain.assetId,
+        symbol: platformChain.asset.symbol,
+        value: BigInt(1_000_000)
+      },
+      {
+        source: jvmChain,
+        destination: platformChain,
+        assetId: jvmChain.assetId,
+        symbol: jvmChain.asset.symbol,
+        value: BigInt(1_000_000)
+      },
+      {
+        source: jvmChain,
+        destination: juneChain,
+        assetId: jvmChain.assetId,
+        symbol: jvmChain.asset.symbol,
+        value: BigInt(1_000_000)
+      }
+    ])(
+      '$#) $value $symbol from $source.name to $destination.name',
+      async ({ source, destination, assetId, value }) => {
+        const operation = new CrossOperation(source, destination, assetId, value)
+        const summary = await mcnAccount.estimate(operation)
+        await mcnAccount.execute(summary)
+        const executable: ExecutableOperation = summary.getExecutable()
+        expect(executable.status).toEqual(DONE_STATUS)
+      },
+      DEFAULT_TIMEOUT
+    )
   })
 
-  describe('Invalid Operations', () => {
+  describe('Invalid execute', () => {
     test.each([
-      ['JUNE-Chain to EUROC1-Chain with negative value', SocotraJUNEChain, SocotraEUROC1Chain, '0x3300000000000000000000000000000000000000', BigInt(-1), InputError],
-      ['JUNE-Chain to EUROC1-Chain with excessive amount', SocotraJUNEChain, SocotraEUROC1Chain, '0x3300000000000000000000000000000000000000', EXCESSIVE_AMOUNT, AccountError],
-      ['JUNE-Chain to EUROC1-Chain with excessive amount and different assetId', SocotraJUNEChain, SocotraEUROC1Chain, SocotraEUROC1AssetId, EXCESSIVE_AMOUNT, AccountError],
-      ['JUNE-Chain to JVM-Chain with zero value', SocotraJUNEChain, SocotraJVMChain, SocotraJUNEAssetId, BigInt(0), InputError],
-      ['JUNE-Chain to Platform-Chain with zero value', SocotraJUNEChain, SocotraPlatformChain, SocotraJUNEAssetId, BigInt(0), InputError],
-      ['JVM-Chain to JUNE-Chain with zero value', SocotraJVMChain, SocotraJUNEChain, SocotraJUNEAssetId, BigInt(0), InputError],
-      ['JVM-Chain to Platform-Chain with zero value', SocotraJVMChain, SocotraPlatformChain, SocotraJUNEAssetId, BigInt(0), InputError],
-      ['Platform-Chain to JUNE-Chain with zero value', SocotraPlatformChain, SocotraJUNEChain, SocotraJUNEAssetId, BigInt(0), InputError],
-      ['Platform-Chain to JVM-Chain with zero value', SocotraPlatformChain, SocotraJVMChain, SocotraJUNEAssetId, BigInt(0), InputError]
-    ])('%s', async (description, src, dest, assetId, value, expectedError) => {
-      const operation = new CrossOperation(src, dest, assetId, value)
-      const summary = await mcnAccount.estimate(operation)
-      await expect(mcnAccount.execute(summary)).rejects.toThrow(expectedError)
-    }, 15000)
+      {
+        source: juneChain,
+        destination: euroChain,
+        assetId: '0x3300000000000000000000000000000000000000',
+        symbol: 'EUROC.e',
+        value: BigInt(-1),
+        expectedError: InputError
+      },
+      {
+        source: juneChain,
+        destination: euroChain,
+        assetId: '0x3300000000000000000000000000000000000000',
+        symbol: 'EUROC.e',
+        value: EXCESSIVE_AMOUNT,
+        expectedError: AccountError
+      },
+      {
+        source: juneChain,
+        destination: euroChain,
+        assetId: euroChain.assetId,
+        symbol: euroChain.asset.symbol,
+        value: EXCESSIVE_AMOUNT,
+        expectedError: AccountError
+      },
+      {
+        source: juneChain,
+        destination: jvmChain,
+        assetId: juneChain.assetId,
+        symbol: juneChain.asset.symbol,
+        value: BigInt(0),
+        expectedError: InputError
+      },
+      {
+        source: juneChain,
+        destination: platformChain,
+        assetId: juneChain.assetId,
+        symbol: juneChain.asset.symbol,
+        value: BigInt(0),
+        expectedError: InputError
+      },
+      {
+        source: jvmChain,
+        destination: juneChain,
+        assetId: jvmChain.assetId,
+        symbol: jvmChain.asset.symbol,
+        value: BigInt(0),
+        expectedError: InputError
+      },
+      {
+        source: jvmChain,
+        destination: platformChain,
+        assetId: jvmChain.assetId,
+        symbol: jvmChain.asset.symbol,
+        value: BigInt(0),
+        expectedError: InputError
+      },
+      {
+        source: platformChain,
+        destination: juneChain,
+        assetId: platformChain.assetId,
+        symbol: platformChain.asset.symbol,
+        value: BigInt(0),
+        expectedError: InputError
+      },
+      {
+        source: platformChain,
+        destination: jvmChain,
+        assetId: platformChain.assetId,
+        symbol: platformChain.asset.symbol,
+        value: BigInt(0),
+        expectedError: InputError
+      }
+    ])(
+      '$#) $value $symbol from $source.name to $destination.name',
+      async ({ source, destination, assetId, value, expectedError }) => {
+        const operation = new CrossOperation(source, destination, assetId, value)
+        const summary = await mcnAccount.estimate(operation)
+        await expect(mcnAccount.execute(summary)).rejects.toThrow(expectedError)
+      },
+      DEFAULT_TIMEOUT
+    )
   })
 })

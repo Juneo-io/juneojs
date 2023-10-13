106b8fba27d19078a3727865d1c765b1945d4573
corentinDallenogare <corentda@hotmail.fr>
2023-10-13 09:43:15 +0200

    add test for bytes

--- tests/unit/utils/bytes.test.ts
+++ tests/unit/utils/bytes.test.ts
@@ -107,97 +107,167 @@ describe('juneojs utils test', () => {
 
     describe('Conversion methods', () => {
       test.each([
-        ['Convert empty buffer to CB58', 32, 'writeUInt8', 0, 'toCB58', 'q'],
-        ['Convert filled buffer to CB58', 32, 'writeUInt8', 255, 'toCB58', 'Hs8Q'],
-        ['Convert empty buffer to CHex', 32, 'writeUInt8', 0, 'toCHex', 'q'],
-        ['Convert filled buffer to CHex', 32, 'writeUInt8', 255, 'toCHex', 'Hs8Q'],
-        ['Convert empty buffer to Hex', 32, 'writeUInt8', 0, 'toHex', 'q'],
-        ['Convert filled buffer to Hex', 32, 'writeUInt8', 255, 'toHex', 'Hs8Q']
+        ['Convert empty buffer to CB58', 32, 'writeUInt8', 0, 'toCB58', '11111111111111111111111111111111LpoYY'],
+        ['Convert filled buffer to CB58', 32, 'writeUInt8', 255, 'toCB58', '2wJdharspTqevWit1s8f7X7E2qGS6ZpsSppTL5Q21vrbT5zzNb'],
+        ['Convert empty buffer to CHex', 32, 'writeUInt8', 0, 'toCHex', '0x00000000000000000000000000000000000000000000000000000000000000000d5f2925'],
+        ['Convert filled buffer to CHex', 32, 'writeUInt8', 255, 'toCHex', '0xff000000000000000000000000000000000000000000000000000000000000006f583350'],
+        ['Convert empty buffer to Hex', 32, 'writeUInt8', 0, 'toHex', '0000000000000000000000000000000000000000000000000000000000000000'],
+        ['Convert filled buffer to Hex', 32, 'writeUInt8', 255, 'toHex', 'ff00000000000000000000000000000000000000000000000000000000000000'],
+        ['Convert empty buffer to CB58', 32, 'writeUInt8', 0, 'toCB58', '11111111111111111111111111111111LpoYY'],
+        ['Convert filled buffer to CB58', 32, 'writeUInt8', 255, 'toCB58', '2wJdharspTqevWit1s8f7X7E2qGS6ZpsSppTL5Q21vrbT5zzNb']
       ])('%s', (description, bufferSize, fillMethod, fillValue, conversionMethod, expectedOutput) => {
         const buffer = JuneoBuffer.alloc(bufferSize);
         (buffer[fillMethod as keyof JuneoBuffer] as any)(fillValue)
-
-        // Utilisation d'une assertion de type pour la méthode de conversion
         expect((buffer[conversionMethod as keyof JuneoBuffer] as any)()).toBe(expectedOutput)
       })
     })
 
-    // 2. Testing getBytes
     describe('getBytes method', () => {
-      test('Should return internal buffer', () => {
-        const buffer = JuneoBuffer.alloc(32)
-        // Fill the buffer somehow
+      test.each([
+        ['Should return internal buffer with length 0', 0],
+        ['Should return internal buffer with length 16', 16],
+        ['Should return internal buffer with length 32', 32],
+        ['Should return internal buffer with length 64', 64]
+      ])('%s', (description, length) => {
+        const buffer = JuneoBuffer.alloc(length)
         expect(buffer.getBytes()).toEqual(buffer.getBytes())
       })
-    })
 
-    // 3. Testing copyOf
-    describe('copyOf method', () => {
-      test('Should copy a subset of buffer', () => {
-        const buffer = JuneoBuffer.alloc(32)
-        // Fill the buffer somehow
-        const copy = buffer.copyOf(0, 16)
-        expect(copy.length).toBe(16)
-      // Additional checks on the copied data
-      })
-
-      test('Should copy the entire buffer', () => {
-        const buffer = JuneoBuffer.alloc(32)
-        // Fill the buffer somehow
-        const copy = buffer.copyOf()
-        expect(copy.length).toBe(32)
-      // Additional checks on the copied data
-      })
-
-      test('Should handle negative indices', () => {
-        const buffer = JuneoBuffer.alloc(32)
-        // Fill the buffer somehow
-        const copy = buffer.copyOf(-4, -1)
-        expect(copy.length).toBe(3)
-      // Additional checks on the copied data
-      })
-    })
-
-    // 4. Testing concat
-    describe('concat method', () => {
-      test('Should concatenate multiple JuneoBuffers', () => {
-        const buffer1 = JuneoBuffer.alloc(16)
-        const buffer2 = JuneoBuffer.alloc(16)
-        const result = JuneoBuffer.concat([buffer1, buffer2])
-        expect(result.length).toBe(32)
-      })
-    })
-
-    // 5. Testing fromBytes
-    describe('fromBytes method', () => {
-      test('Should create a new JuneoBuffer from a given buffer', () => {
-        const someBuffer = Buffer.from([/* Some byte values */])
-        const buffer = JuneoBuffer.fromBytes(someBuffer)
-        expect(buffer.length).toBe(someBuffer.length)
-      })
-    })
-
-    // 6. Testing fromString
-    describe('fromString method', () => {
-      test('Should create a new JuneoBuffer from a hex string', () => {
-        const buffer = JuneoBuffer.fromString('68656c6c6f', 'hex') // 'hello' in hex
-        expect(buffer.length).toBe(5)
-      })
-
-      test('Should throw error for invalid data', () => {
+      test.each([
+        ['Should fail with negative length -1', -1],
+        ['Should fail with negative length -16', -16],
+        ['Should fail with non-numeric length "abc"', 'abc'],
+        ['Should fail with null length', null],
+        ['Should fail with undefined length', undefined]
+      ])('%s', (description, length) => {
         expect(() => {
-          JuneoBuffer.fromString('invalid', 'hex')
+          JuneoBuffer.alloc(length as any)
         }).toThrow()
       })
     })
 
-    // 7. Testing comparator
+    describe('copyOf method', () => {
+      test.each([
+        ['Should copy the entire buffer', 32, 0, 32, 32],
+        ['Should handle negative indices', 32, -4, -1, 3],
+        ['Should copy a subset of buffer from 0 to 16', 32, 0, 16, 16],
+        ['Should copy the entire buffer when indices are not provided', 32, undefined, undefined, 32],
+        ['Should handle negative indices from -4 to -1', 32, -4, -1, 3],
+        ['Should handle out-of-bounds indices from 40 to 50', 32, 40, 50, 0]
+
+      ])('%s', (description, bufferSize, start, end, expectedLength) => {
+        const buffer = JuneoBuffer.alloc(bufferSize)
+        const copy = buffer.copyOf(start, end)
+        expect(copy.length).toBe(expectedLength)
+      })
+
+      // All this test does not throw an error
+      test.each([
+        ['Should fail with negative length', -32, 0, 16],
+        ['Should fail with start greater than end', 32, 16, 0],
+        ['Should fail when negative start index is out-of-bounds', 32, -40, -30],
+        ['Should fail when end index is less than start index', 32, 10, 5],
+        ['Should fail with both start and end negative where start > end', 32, -1, -4],
+        ['Should fail with start as undefined', 32, undefined, 16],
+        ['Should fail with end as undefined', 32, 0, undefined],
+        ['Should fail with start as null', 32, null, 16],
+        ['Should fail with end as null', 32, 0, null]
+      ])('%s', (description, bufferSize, start, end) => {
+        const buffer = JuneoBuffer.alloc(bufferSize)
+        expect(() => {
+          buffer.copyOf(start as any, end as any)
+        }).toThrow()
+      })
+    })
+
+    describe('concat method', () => {
+      test.each([
+        ['Should concatenate two buffers of length 16', [16, 16], 32],
+        ['Should concatenate three buffers of length 16', [16, 16, 16], 48],
+        ['Should concatenate one buffer of length 16', [16], 16],
+        ['Should handle an empty array of buffers', [], 0]
+      ])('%s', (description, bufferSizes, expectedLength) => {
+        const buffers = bufferSizes.map(size => JuneoBuffer.alloc(size))
+        const result = JuneoBuffer.concat(buffers)
+        expect(result.length).toBe(expectedLength)
+      })
+
+      test.each([
+        ['Should fail when array contains non-JuneoBuffer elements', [JuneoBuffer.alloc(16), 'invalid'], 16],
+        ['Should fail when array contains null elements', [JuneoBuffer.alloc(16), null], 16]
+      ])('%s', (description, invalidBuffers, expectedLength) => {
+        expect(() => {
+          JuneoBuffer.concat(invalidBuffers as any)
+        }).toThrow()
+      })
+    })
+
+    describe('fromBytes method', () => {
+      test.each([
+        ['Should create a new JuneoBuffer from an empty buffer', Buffer.from([]), 0],
+        ['Should create a new JuneoBuffer from a non-empty buffer', Buffer.from([1, 2, 3, 4]), 4],
+        ['Should create a new JuneoBuffer from a large buffer', Buffer.from(Array(100).fill(1)), 100],
+        ['Should create a new JuneoBuffer from a buffer with various values', Buffer.from([255, 128, 64, 32]), 4]
+      ])('%s', (description, someBuffer, expectedLength) => {
+        const buffer = JuneoBuffer.fromBytes(someBuffer)
+        expect(buffer.length).toBe(expectedLength)
+      })
+
+      test.each([
+        ['Should fail when provided with a non-Buffer object', 'notABuffer', 0],
+        ['Should fail when provided with a null object', null, 0],
+        ['Should fail when provided with an undefined object', undefined, 0]
+      ])('%s', (description, someBuffer, expectedLength) => {
+        expect(() => {
+          JuneoBuffer.fromBytes(someBuffer as any)
+        }).toThrow()
+      })
+    })
+
+    describe('fromString method', () => {
+      test.each([
+        ['Should create a new JuneoBuffer from a non-empty hex string', '68656c6c6f', 'hex', 5], // 'hello' in hex
+        ['Should create a new JuneoBuffer from a hex string with various values', 'deadbeef', 'hex', 4],
+        ['Should create a new JuneoBuffer from a cHex string with various values', '3c647d88Bc92766075feA7A965CA599CAAB2FD26', 'hex', 20],
+        ['Should create a new JuneoBuffer from a non-empty CB58 string', '2wJdharspTqevWit1s8f7X7E2qGS6ZpsSppTL5Q21vrbT5zzNb', 'CB58', 32]
+      ])('%s', (description, someString, encoding, expectedLength) => {
+        const buffer = JuneoBuffer.fromString(someString, encoding)
+        expect(buffer.length).toBe(expectedLength)
+      })
+
+      test.each([
+        ['Should not create a new JuneoBuffer from an empty hex string', '', 'hex']
+      ])('%s', (description, someString, encoding) => {
+        expect(() => {
+          JuneoBuffer.fromString(someString, encoding)
+        }).toThrow()
+      })
+    })
+
     describe('comparator method', () => {
-      test('Should sort JuneoBuffers correctly', () => {
-        const buffer1 = JuneoBuffer.alloc(16)
-        const buffer2 = JuneoBuffer.alloc(16)
-        // Fill the buffers somehow so they can be compared
-        expect(JuneoBuffer.comparator(buffer1, buffer2)).toBe(0)
+      test.each([
+        ['Should return 0 when both buffers have the same data', 16, 16, 0],
+        ['Should return a negative number when the first buffer is less than the second', 15, 16, -1],
+        ['Should return a positive number when the first buffer is greater than the second', 17, 16, 1]
+      ])('%s', (description, bufferSize1, bufferSize2, expectedComparison) => {
+        const buffer1 = JuneoBuffer.alloc(bufferSize1 as any)
+        const buffer2 = JuneoBuffer.alloc(bufferSize2 as any)
+        expect(JuneoBuffer.comparator(buffer1, buffer2)).toBe(expectedComparison)
+      })
+
+      test.each([
+        ['Should fail when both buffers are undefined', undefined, undefined],
+        ['Should fail when first buffer is undefined', undefined, 16],
+        ['Should fail when second buffer is undefined', 16, undefined],
+        ['Should fail when both buffers are null', null, null],
+        ['Should fail when first buffer is null', null, 16],
+        ['Should fail when second buffer is null', 16, null]
+      ])('%s', (description, bufferSize1, bufferSize2) => {
+        expect(() => {
+          const buffer1 = JuneoBuffer.alloc(bufferSize1 as any)
+          const buffer2 = JuneoBuffer.alloc(bufferSize2 as any)
+          JuneoBuffer.comparator(buffer1, buffer2)
+        }).toThrow()
       })
     })
   })

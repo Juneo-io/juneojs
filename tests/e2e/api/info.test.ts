a8f7b186f717172ba14a31f67168339c047c0c1a
alekswaslet <1346150+alekswaslet@users.noreply.github.com>
2023-10-13 17:51:39 +0200

    Updated InfoAPI tests

--- tests/e2e/api/info.test.ts
+++ tests/e2e/api/info.test.ts
@@ -1,101 +1,108 @@
 import { MCNProvider, SocotraJUNEChain, SocotraNetwork } from '../../../src'
 
-describe('InfoAPI tests', () => {
-  const provider: MCNProvider = new MCNProvider()
+describe('InfoAPI', () => {
+  const provider: MCNProvider = new MCNProvider(SocotraNetwork)
 
   describe('isBootstrapped', () => {
-    test('Return true for a bootstrapped chain', async () => {
-      const result = await provider.info.isBootstrapped(SocotraJUNEChain.id)
+    test.each([{ chainID: SocotraJUNEChain.id }])('Valid: $chainID', async ({ chainID }) => {
+      const result = await provider.info.isBootstrapped(chainID)
       expect(result.isBootstrapped).toEqual(true)
     })
-    test.failing('Wrong chain id', async () => {
-      await provider.info.isBootstrapped('WrongChainId')
+    test.failing.each([{ chainID: 'WRONG_CHAIN_ID' }])('Invalid: $chainID', async ({ chainID }) => {
+      await provider.info.isBootstrapped('WRONG_CHAIN_ID')
     })
   })
 
   describe('getBlockchainID', () => {
-    test('Return correct Blockchain ID', async () => {
-      const result = await provider.info.getBlockchainID('JUNE')
+    test.each([{ alias: 'JUNE' }])('Valid: $alias', async ({ alias }) => {
+      const result = await provider.info.getBlockchainID(alias)
       expect(result.blockchainID).toEqual(SocotraJUNEChain.id)
     })
-    test.failing('Not return correct Blockchain ID', async () => {
-      await provider.info.getBlockchainID('WrongAlias')
+    test.failing.each([{ alias: 'WRONG_ALIAS' }])('Invalid: $alias', async ({ alias }) => {
+      await provider.info.getBlockchainID('alias')
     })
   })
 
   describe('getNetworkID', () => {
-    test('Return correct Network ID', async () => {
+    test('Returns correct network id', async () => {
       const result = await provider.info.getNetworkID()
-      expect(result.networkID).toEqual((SocotraNetwork.id).toString())
+      expect(result.networkID).toEqual(provider.mcn.id.toString())
     })
   })
 
   describe('getNetworkName', () => {
-    test('Return correct Network Name', async () => {
+    test('Returns correct network name', async () => {
       const result = await provider.info.getNetworkName()
-      expect(result.networkName).toEqual('socotra')
+      expect(result.networkName).toEqual(provider.mcn.hrp)
     })
   })
 
   describe('getNodeID', () => {
-    test('Return correct Node ID', async () => {
+    test('Returns node id', async () => {
       const result = await provider.info.getNodeID()
-      expect((result)).toBeDefined()
+      expect(result).toBeDefined()
     })
   })
 
   describe('getNodeIP', () => {
-    test('Return correct Node IP', async () => {
+    test('Returns node ip', async () => {
       const result = await provider.info.getNodeIP()
       expect(result).toBeDefined()
     })
   })
 
   describe('getNodeVersion', () => {
-    test('Return correct Node Version', async () => {
+    test('Returns node version', async () => {
       const result = await provider.info.getNodeVersion()
       expect(result).toBeDefined()
     })
   })
 
   describe('getTxFee', () => {
-    test('Return correct Transaction Fee', async () => {
+    test('Returns fee config', async () => {
       const result = await provider.info.getTxFee()
       expect(result).toBeDefined()
     })
-    test('Return correct Transaction Fee force update', async () => {
+    test('Returns cached fee config', async () => {
+      const result = await provider.info.getTxFee()
+      expect(result).toBeDefined()
+    })
+    test('Returns updated fee config', async () => {
       const result = await provider.info.getTxFee(true)
       expect(result).toBeDefined()
     })
   })
 
   describe('getVMs', () => {
-    test('Return correct list of VMs', async () => {
+    test('Returns installed VMs', async () => {
       const result = await provider.info.getVMs()
       expect(result).toBeDefined()
     })
   })
 
   describe('peers', () => {
-    test('Return correct list of peers', async () => {
+    test('Returns list of peers', async () => {
       const result = await provider.info.peers()
       expect(result).toBeDefined()
     })
-    test.failing('Wrong node id', async () => {
-      await provider.info.peers(['WrongNodeId', 'NodeID-P1ESFUf8tutmR8hszZUWsXAJEKARZ5SPw'])
-    })
-    test.failing('Wrong node id', async () => {
-      await provider.info.peers(['NodeID-P1ESFUf8tutmR8hszZUWsXAJEKARZ5SPa'])
+    test.failing.each([
+      { nodeIDs: ['WRONG_NODE_ID', 'NodeID-P1ESFUf8tutmR8hszZUWsXAJEKARZ5SPw'] },
+      { nodeIDs: ['NodeID-P1ESFUf8tutmR8hszZUWsXAJEKARZ5SPa'] }
+    ])('Invalid: $nodeIDs', async ({ nodeIDs }) => {
+      await provider.info.peers(nodeIDs)
     })
   })
 
   describe('uptime', () => {
-    test('Return correct uptime information', async () => {
-      const result = await provider.info.uptime()
-      expect(result).toBeDefined()
-    })
-    test.failing('Wrong supernet id', async () => {
-      await provider.info.uptime('WrongSupernetId')
+    test.each([{ supernetID: undefined }, { supernetID: provider.mcn.primary.id }])(
+      'Valid: $supernetID',
+      async ({ supernetID }) => {
+        const result = await provider.info.uptime(supernetID)
+        expect(result).toBeDefined()
+      }
+    )
+    test.failing.each([{ supernetID: 'WRONG_SUPERNET_ID' }])('Invalid: $supernetID', async ({ supernetID }) => {
+      await provider.info.uptime(supernetID)
     })
   })
 })

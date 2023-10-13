a81fed93cdda5270877ac4b1c113d1edc118ab07
alekswaslet <1346150+alekswaslet@users.noreply.github.com>
2023-10-13 17:21:40 +0200

    Updated JEVMAPI tests

--- tests/e2e/api/jevm.test.ts
+++ tests/e2e/api/jevm.test.ts
@@ -1,12 +1,10 @@
-import { MCNProvider, SocotraEUROC1AssetId, SocotraJUNEChain } from '../../../src'
+import { MCNProvider, SocotraEUROC1Asset, SocotraJUNEChain, SocotraNetwork } from '../../../src'
 
-describe('JEVMAPI tests', () => {
-  const provider: MCNProvider = new MCNProvider()
+describe('JEVMAPI', () => {
+  const provider: MCNProvider = new MCNProvider(SocotraNetwork)
 
   describe('getTx', () => {
-    test.each([
-      ['241mEKvJetzAbVxvSsooEaAYgXkaipSDuxEoXBxBDP8mKHb8Cm']
-    ])('Correct transaction for txID', async (txID) => {
+    test.each([{ txID: '241mEKvJetzAbVxvSsooEaAYgXkaipSDuxEoXBxBDP8mKHb8Cm' }])('Valid: $txID', async ({ txID }) => {
       const result = await provider.jevm[SocotraJUNEChain.id].getTx(txID)
       expect(result.blockHeight).toBeDefined()
       expect(result.encoding).toBeDefined()
@@ -14,19 +12,17 @@ describe('JEVMAPI tests', () => {
     })
 
     test.failing.each([
-      ['241mEKvJjtzAbVxvSsooEaAYgXkaipSDuxEoXBxBDP8mKHb8Cm'],
-      ['0x3c529e9941b6ca0ec34948c7f797e94ff810643ef64896c409ea0df36be9e554'],
-      ['wrongTxID']
-    ])('not gettransaction for txID', async (txID) => {
+      { txID: '241mEKvJjtzAbVxvSsooEaAYgXkaipSDuxEoXBxBDP8mKHb8Cm' },
+      { txID: '0x3c529e9941b6ca0ec34948c7f797e94ff810643ef64896c409ea0df36be9e554' },
+      { txID: 'INVALID_TX_ID' }
+    ])('Invalid: $txID', async ({ txID }) => {
       const result = await provider.jevm[SocotraJUNEChain.id].getTx(txID)
       expect(result.tx).toBeDefined()
     })
   })
 
   describe('getTxStatus', () => {
-    test.each([
-      ['241mEKvJetzAbVxvSsooEaAYgXkaipSDuxEoXBxBDP8mKHb8Cm']
-    ])('Status Accepted for txID', async (txID) => {
+    test.each([{ txID: '241mEKvJetzAbVxvSsooEaAYgXkaipSDuxEoXBxBDP8mKHb8Cm' }])('Valid: $txID', async ({ txID }) => {
       const result = await provider.jevm[SocotraJUNEChain.id].getTxStatus(txID)
       expect(result.status).toEqual('Accepted')
       expect(result.blockHeight).toEqual('4976404')
@@ -39,39 +35,57 @@ describe('JEVMAPI tests', () => {
 
   describe('eth_getAssetBalance', () => {
     test.each([
-      ['0x9b31d8C5Dd49fCdE96218895f96a6eC1ea894529', '0x4BF7C5', SocotraEUROC1AssetId, BigInt(204427130n)]
-    ])('should return correct asset balance', async (address, block, assetID, expectedBalance) => {
-      const result = await provider.jevm[SocotraJUNEChain.id].eth_getAssetBalance(address, block, assetID)
+      {
+        address: '0x9b31d8C5Dd49fCdE96218895f96a6eC1ea894529',
+        block: '0x4BF7C5',
+        asset: SocotraEUROC1Asset,
+        expectedBalance: BigInt(204_427_130n)
+      }
+    ])('$asset.symbol balance of $address at block $block', async ({ address, block, asset, expectedBalance }) => {
+      const result = await provider.jevm[SocotraJUNEChain.id].eth_getAssetBalance(address, block, asset.assetId)
       expect(result).toEqual(expectedBalance)
     })
 
-    describe('eth_getAssetBalance failing tests', () => {
-      test.failing.each([
-        ['wrong address', '0x9b31d8C5Dd49fCdE96218895f96a6eC894529', '0x4BF7C5', SocotraEUROC1AssetId, null],
-        ['wrong block', '0x9b31d8C5Dd49fCdE96218895f96a6eC1ea894529', '2000', SocotraEUROC1AssetId, null],
-        ['wrong assetID', '0x9b31d8C5Dd49fCdE96218895f96a6eC1ea894529', '0x4BF7C5', '0x0', null]
-      ])('%s', async (description, address, block, assetID) => {
-        await provider.jevm[SocotraJUNEChain.id].eth_getAssetBalance(address, block, assetID)
-      })
+    test.failing.each([
+      {
+        description: 'Wrong address',
+        address: '0x9b31d8C5Dd49fCdE96218895f96a6eC894529',
+        block: '0x4BF7C5',
+        assetID: SocotraEUROC1Asset.assetId
+      },
+      {
+        description: 'Wrong block',
+        address: '0x9b31d8C5Dd49fCdE96218895f96a6eC1ea894529',
+        block: '2000',
+        assetID: SocotraEUROC1Asset.assetId
+      },
+      {
+        description: 'Wrong assetID',
+        address: '0x9b31d8C5Dd49fCdE96218895f96a6eC1ea894529',
+        block: '0x4BF7C5',
+        assetID: '0x0'
+      }
+    ])('$description: $address, $block, $assetID', async ({ address, block, assetID }) => {
+      await provider.jevm[SocotraJUNEChain.id].eth_getAssetBalance(address, block, assetID)
     })
   })
 
   describe('eth_baseFee', () => {
-    test('should return correct base fee', async () => {
+    test('Returns base fee', async () => {
       const result = await provider.jevm[SocotraJUNEChain.id].eth_baseFee()
       expect(result).toBeDefined()
     })
   })
 
   describe('eth_maxPriorityFeePerGas', () => {
-    test('should return correct max priority fee per gas', async () => {
+    test('Returns max priority fee per gas', async () => {
       const result = await provider.jevm[SocotraJUNEChain.id].eth_maxPriorityFeePerGas()
       expect(result).toBeDefined()
     })
   })
 
   describe('eth_getChainConfig', () => {
-    test('should return correct chain config', async () => {
+    test('Returns chain config', async () => {
       const result = await provider.jevm[SocotraJUNEChain.id].eth_getChainConfig()
       expect(result).toBeDefined()
     })
@@ -79,8 +93,12 @@ describe('JEVMAPI tests', () => {
 
   describe('eth_getBalance', () => {
     test.each([
-      ['0x9b31d8C5Dd49fCdE96218895f96a6eC1ea894529', '0x4BF7C5', BigInt(38623609074512286678n)]
-    ])('should return correct balance', async (address, block, expectedBalance) => {
+      {
+        address: '0x9b31d8C5Dd49fCdE96218895f96a6eC1ea894529',
+        block: '0x4BF7C5',
+        expectedBalance: BigInt(38_623609074_512286678n)
+      }
+    ])('Valid from $address at block $block', async ({ address, block, expectedBalance }) => {
       const result = await provider.jevm[SocotraJUNEChain.id].eth_getBalance(address, block)
       expect(result).toEqual(expectedBalance)
     })
@@ -88,8 +106,8 @@ describe('JEVMAPI tests', () => {
 
   describe('eth_getTransactionCount', () => {
     test.each([
-      ['0x9b31d8C5Dd49fCdE96218895f96a6eC1ea894529', '0x4BF7C5', BigInt(511)]
-    ])('should return correct transaction count', async (address, block, expectedCount) => {
+      { address: '0x9b31d8C5Dd49fCdE96218895f96a6eC1ea894529', block: '0x4BF7C5', expectedCount: BigInt(511) }
+    ])('Valid from $address at block $block', async ({ address, block, expectedCount }) => {
       const result = await provider.jevm[SocotraJUNEChain.id].eth_getTransactionCount(address, block)
       expect(result).toEqual(expectedCount)
     })

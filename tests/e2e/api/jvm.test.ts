f89fac5afb9a828edab65a696fe5214ebc649598
alekswaslet <1346150+alekswaslet@users.noreply.github.com>
2023-10-13 16:57:45 +0200

    Updated JVMAPI tests

--- tests/e2e/api/jvm.test.ts
+++ tests/e2e/api/jvm.test.ts
@@ -1,32 +1,41 @@
-import { MCNProvider, SocotraBCH1AssetId, SocotraETC1AssetId, SocotraJUNEAssetId, type GetAssetDescriptionResponse, type GetBlockResponse, type GetHeightResponse, type GetTxResponse } from '../../../src'
+import {
+  MCNProvider,
+  type GetAssetDescriptionResponse,
+  type GetBlockResponse,
+  type GetHeightResponse,
+  type GetTxResponse,
+  SocotraNetwork,
+  SocotraJUNEAsset,
+  SocotraETH1Asset,
+  SocotraBCH1Asset
+} from '../../../src'
 
-describe('JVMAPI tests', () => {
-  const provider: MCNProvider = new MCNProvider()
+describe('JVMAPI', () => {
+  const provider: MCNProvider = new MCNProvider(SocotraNetwork)
 
   describe('buildGenesis', () => {
     // TODO later
   })
 
   describe('getAssetDescription', () => {
-    test.each([
-      [SocotraJUNEAssetId, 'JUNE'],
-      [SocotraETC1AssetId, 'Ethereum Classic'],
-      [SocotraBCH1AssetId, 'Bitcoin Cash']
-    ])('Asset description for %s', async (assetId, expectedName) => {
-      const result: GetAssetDescriptionResponse = await provider.jvm.getAssetDescription(assetId)
-      expect(result.name).toEqual(expectedName)
-      expect(result.symbol).toBeDefined()
-      expect(result.denomination).toBeDefined()
-      expect(result.assetID).toBeDefined()
-    })
+    test.each([{ asset: SocotraJUNEAsset }, { asset: SocotraETH1Asset }, { asset: SocotraBCH1Asset }])(
+      'Valid: $asset.assetId ($asset.symbol)',
+      async ({ asset }) => {
+        const result: GetAssetDescriptionResponse = await provider.jvm.getAssetDescription(asset.assetId)
+        // TODO in Socotra2 use asset name
+        expect(result.name).toBeDefined()
+        expect(result.symbol).toEqual(asset.symbol)
+        expect(result.denomination).toEqual(asset.decimals.toString())
+        expect(result.assetID).toEqual(asset.assetId)
+      }
+    )
 
     test.failing.each([
-      ['SocotraJUNEAssetId', 'JUNE'],
-      ['2RcLCZTsxSnvzeBvtrjRo8PCzLXuecHBoyr8DNp1R8ob8kHkbZ', 'Ethereum Classic'],
-      ['fakeAssetId', 'Bitcoin Cash']
-    ])('Asset description for %s', async (assetId, expectedName) => {
+      { assetId: '2RcLCZTsxSnvzeBvtrjRo8PCzLXuecHBoyr8DNp1R8ob8kHkbZ' },
+      { assetId: 'INVALID_ASSET_ID' }
+    ])('Invalid: $assetId', async ({ assetId }) => {
       const result: GetAssetDescriptionResponse = await provider.jvm.getAssetDescription(assetId)
-      expect(result.name).toEqual(expectedName)
+      expect(result.name).toBeDefined()
     })
   })
 
@@ -35,30 +44,27 @@ describe('JVMAPI tests', () => {
   })
 
   describe('getBlockByHeight', () => {
-    test.each([
-      ['should be defined for height 1', 1],
-      ['should be defined for height 10', 10],
-      ['should be defined for height 100', 100],
-      ['should handle null input', null],
-      ['should handle undefined input', undefined]
-    ])('%s', async (description, height) => {
-      const result: GetBlockResponse = await provider.jvm.getBlockByHeight(height as any)
-      expect(result.block).toBeDefined()
-      expect(result.encoding).toBeDefined()
-    })
+    test.each([{ height: 1 }, { height: 10 }, { height: 100 }, { height: null }, { height: undefined }])(
+      'Valid: $height',
+      async ({ height }) => {
+        const result: GetBlockResponse = await provider.jvm.getBlockByHeight(height as any)
+        expect(result.block).toBeDefined()
+        expect(result.encoding).toBeDefined()
+      }
+    )
 
     test.failing.each([
-      ['should handle negative height', -1],
-      ['should handle string input', 'aString'],
-      ['should handle object input', {}],
-      ['should handle array input', []]
-    ])('%s', async (description, height) => {
+      { description: 'Negative height', height: -1 },
+      { description: 'String input', height: 'aString' },
+      { description: 'Object input', height: {} },
+      { description: 'Array input', height: [] }
+    ])('$description: $height', async ({ height }) => {
       await provider.jvm.getBlockByHeight(height as any)
     })
   })
 
   describe('getHeight', () => {
-    test('Should return height', async () => {
+    test('Returns height', async () => {
       const result: GetHeightResponse = await provider.jvm.getHeight()
       expect(result.height).toBeDefined()
     })
@@ -66,22 +72,22 @@ describe('JVMAPI tests', () => {
 
   describe('getTx', () => {
     test.each([
-      ['should be defined for valid txID', 'dGJVWGj3GHQRAvt87xqcVUwKNKcJRaB7iUwGpNP9PYSrk6rie'],
-      ['should be defined for valid txID', '2FKNX3WoJwtbanNxVV44qaXsv8SgkiBtD4psHC2wdbLizXvGS']
-    ])('%s', async (description, txID) => {
+      { txID: 'dGJVWGj3GHQRAvt87xqcVUwKNKcJRaB7iUwGpNP9PYSrk6rie' },
+      { txID: '2FKNX3WoJwtbanNxVV44qaXsv8SgkiBtD4psHC2wdbLizXvGS' }
+    ])('Valid: $txID', async ({ txID }) => {
       const result: GetTxResponse = await provider.jvm.getTx(txID)
       expect(result.encoding).toBeDefined()
       expect(result.tx).toBeDefined()
     })
 
     test.failing.each([
-      ['should handle invalid txID', 'invalidTxID'],
-      ['should handle numerical string', '123'],
-      ['should handle null input', null],
-      ['should handle undefined input', undefined],
-      ['should handle object input', {}],
-      ['should handle array input', []]
-    ])('%s', async (description, txID) => {
+      { description: 'Invalid txID', txID: 'INVALID_TX_ID' },
+      { description: 'Numerical string', txID: '123' },
+      { description: 'Null input', txID: null },
+      { description: 'Undefined input', txID: undefined },
+      { description: 'Object input', txID: {} },
+      { description: 'Array input', txID: [] }
+    ])('$description: $txID', async ({ txID }) => {
       await provider.jvm.getTx(txID as any)
     })
   })

450ed3632a3d3965f02a33ccfc31dac36f23a0fc
corentinDallenogare <corentda@hotmail.fr>
2023-10-23 15:31:31 +0200

    add test for input and transaction

--- tests/unit/transaction/input.test.ts
+++ tests/unit/transaction/input.test.ts
@@ -80,6 +80,7 @@ describe('Secp256k1Input', () => {
       expect(serialized.toHex()).toBe(expectedValue)
     })
 })
+
 describe('TransferableInput', () => {
   test.each([
     {
@@ -97,6 +98,54 @@ describe('TransferableInput', () => {
       assetId: new AssetId('xNScMRJEmvx8A34Df2gQkTSjLMqWDcPbaPYfPYJRuf1cjLHLM'),
       input: new Secp256k1Input(BigInt(200), [2]),
       expectedSerializedValue: '636ded20c23c2b2bba3a6a46ace3811da6eda26c39fe8338f94c63c31e5b6962000000027db94035fa4902aee2c4c1e600d4e4d9cd9121ca57d23e98eeab45bdf88de8b50000000500000000000000c80000000100000002'
+    },
+    {
+      description: 'Multiple address indices',
+      transactionId: new TransactionId('tYsq6e6ppF91b1PoYKN2iEo69X7aMJRJyv85jcb6vi2QKs21W'),
+      utxoIndex: 3,
+      assetId: new AssetId('fawNQXm5Q8AzyvuvASN1NfvYuqqmvr55WQeD8ZibJz6Q12WP4'),
+      input: new Secp256k1Input(BigInt(300), [1, 2]),
+      expectedSerializedValue: '750cb6424ff20ce3af51d1e79c4f70436de20cea2fffb8a370e1e191fdad04ce00000003579cbc918f783fe6a7fd30e0b7210ad7cbdbbd1808c2ba7c835a1a96cf5d7e1700000005000000000000012c000000020000000100000002'
+    },
+    {
+      description: 'Zero UTXO index',
+      transactionId: new TransactionId('2kArSUBdZGoRMDBvoZLyGGdK3yoVjtsTV3GWHSsic6GRVMuArH'),
+      utxoIndex: 0,
+      assetId: new AssetId('2biwSVXZ8KMkwJbYnC4M6khf759pgr49ZPL4M1k8zvYjeQmG82'),
+      input: new Secp256k1Input(BigInt(400), [3]),
+      expectedSerializedValue: 'e5b7f3ccdca9f8785bc2332964f2c8186ac22b98879397e32ff6cbc261d4aa5900000000d28a0223d17697e48048cc00004aabc6736b68f853b026c50a59f3132e418a050000000500000000000001900000000100000003'
+    },
+    {
+      description: 'Empty address indices',
+      transactionId: new TransactionId('gS845TcHmTDPMDU8z9E2bAZ7ujp9F7CAReQqCvSZh1Pvb6mGH'),
+      utxoIndex: 4,
+      assetId: new AssetId('2biwSVXZ8KMkwJbYnC4M6khf759pgr49ZPL4M1k8zvYjeQmG82'),
+      input: new Secp256k1Input(BigInt(0), []),
+      expectedSerializedValue: '5989a9dcfbdd73148a327e867954a3876755f84a6c8049088dacc5d070eb40fb00000004d28a0223d17697e48048cc00004aabc6736b68f853b026c50a59f3132e418a0500000005000000000000000000000000'
+    },
+    {
+      description: 'High amount',
+      transactionId: new TransactionId('tYsq6e6ppF91b1PoYKN2iEo69X7aMJRJyv85jcb6vi2QKs21W'),
+      utxoIndex: 6,
+      assetId: new AssetId('2biwSVXZ8KMkwJbYnC4M6khf759pgr49ZPL4M1k8zvYjeQmG82'),
+      input: new Secp256k1Input(BigInt('9007199254740991'), [5]),
+      expectedSerializedValue: '750cb6424ff20ce3af51d1e79c4f70436de20cea2fffb8a370e1e191fdad04ce00000006d28a0223d17697e48048cc00004aabc6736b68f853b026c50a59f3132e418a0500000005001fffffffffffff0000000100000005'
+    },
+    {
+      description: 'Multiple address indices with high amount',
+      transactionId: new TransactionId('gS845TcHmTDPMDU8z9E2bAZ7ujp9F7CAReQqCvSZh1Pvb6mGH'),
+      utxoIndex: 7,
+      assetId: new AssetId('XgN1Q8XeEuj9SU5UtM2LtNWsufgrz6amVs8BwGcJZM7ZuarNq'),
+      input: new Secp256k1Input(BigInt('9007199254740991'), [5, 6]),
+      expectedSerializedValue: '5989a9dcfbdd73148a327e867954a3876755f84a6c8049088dacc5d070eb40fb0000000745a8de9ee8e1300fa74cd67b462c2eaf93faa7f415645ebaddfe936e5cf56f9300000005001fffffffffffff000000020000000500000006'
+    },
+    {
+      description: 'Zero amount with multiple address indices',
+      transactionId: new TransactionId('qp19AULkpQwGwPJpfkMjytCzfyM3Bm3pMwBXfQmNbgimzutJQ'),
+      utxoIndex: 8,
+      assetId: new AssetId('XgN1Q8XeEuj9SU5UtM2LtNWsufgrz6amVs8BwGcJZM7ZuarNq'),
+      input: new Secp256k1Input(BigInt(0), [7, 8]),
+      expectedSerializedValue: '6ed477de5bee3b3d5813b341e249b1b48747ef2c3bcce716cafeb2f43fb7031a0000000845a8de9ee8e1300fa74cd67b462c2eaf93faa7f415645ebaddfe936e5cf56f93000000050000000000000000000000020000000700000008'
     }
   ])('$description', ({ transactionId, utxoIndex, assetId, input, expectedSerializedValue }) => {
     const transferableInput = new TransferableInput(transactionId, utxoIndex, assetId, input as TransactionInput & Serializable)

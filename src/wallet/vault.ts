0d2fe3e1beef49337ee9d97f7d7e6fef88df204e
alekswaslet <1346150+alekswaslet@users.noreply.github.com>
2024-02-02 20:29:53 +0100

    Completed MCNVault

--- src/wallet/vault.ts
+++ src/wallet/vault.ts
@@ -1,18 +1,67 @@
+import { type MCNProvider } from '../juneo'
 import { type MCNAccount } from './account'
 
 export class MCNVault {
-  mainAccount: MCNAccount
-  accounts: MCNAccount[] = []
+  private readonly provider: MCNProvider
+  private mainAccount: MCNAccount
+  private readonly accounts = new Map<string, MCNAccount>()
 
-  constructor (mainAccount: MCNAccount) {
+  constructor (provider: MCNProvider, mainAccount: MCNAccount, accounts: MCNAccount[]) {
+    this.provider = provider
     this.mainAccount = mainAccount
+    if (!this.hasAccount(mainAccount)) {
+      this.addAccount(mainAccount)
+    }
+    this.addAccounts(accounts)
   }
 
   addAccount (account: MCNAccount): void {
-    this.accounts.push(account)
+    this.accounts.set(MCNVault.getAccountId(this.provider, account), account)
+  }
+
+  addAccounts (accounts: MCNAccount[]): void {
+    for (const account of accounts) {
+      this.addAccount(account)
+    }
   }
 
   setMainAccount (account: MCNAccount): void {
     this.mainAccount = account
+    if (!this.hasAccount(account)) {
+      this.addAccount(account)
+    }
+  }
+
+  hasAccount (account: MCNAccount): boolean {
+    return this.accounts.has(MCNVault.getAccountId(this.provider, account))
+  }
+
+  getAccountWithAddress (address: string): MCNAccount | undefined {
+    for (const account of this.accounts.values()) {
+      for (const chainAccount of account.chainAccounts.values()) {
+        if (chainAccount.address === address) {
+          return account
+        }
+      }
+    }
+    return undefined
+  }
+
+  getMainAccount (): MCNAccount {
+    return this.mainAccount
+  }
+
+  getAddresses (chainId: string): string[] {
+    const addresses: string[] = []
+    for (const account of this.accounts.values()) {
+      addresses.push(account.getAccount(chainId).address)
+    }
+    return addresses
+  }
+
+  private static getAccountId (provider: MCNProvider, account: MCNAccount): string {
+    const jvmId: string = account.getAccount(provider.jvm.chain.id).chainWallet.getKeyPair().publicKey
+    const evmId: string = account.getAccount(provider.june.chain.id).chainWallet.getKeyPair().publicKey
+    return `${jvmId}_${evmId}`
   }
 }

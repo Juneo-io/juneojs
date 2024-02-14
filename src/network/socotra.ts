01b818fb3088f066f67af885a45c8e7a3033d092
alekswaslet <1346150+alekswaslet@users.noreply.github.com>
2024-02-14 18:13:50 +0100

    Updated RPC address

--- src/network/socotra.ts
+++ src/network/socotra.ts
@@ -13,7 +13,7 @@ const SocotraStakeConfig: StakeConfig = new StakeConfig(
   BigInt(24 * 3600),
   BigInt(365 * 24 * 3600)
 )
-const SocotraAddress: string = 'https://api.socotra-test.network:9650'
+const SocotraAddress: string = 'https://api.genesis-testnet.network/ext/bc/JUNE/rpc'
 
 export const SocotraJUNEAssetId: string = '2RcLCZTsxSnvzeBvtrjRo8PCzLXuecHBoyr8DNp1R8ob8kHkbR'
 export const SocotraETH1AssetId: string = 'JeUvxJPXoL3EtVGSPwtyYXVymxGbpcBcaa2Kq7TCB43HsKMAS'

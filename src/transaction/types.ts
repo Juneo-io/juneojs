ee0ca14f2a6cfe1c63982d03d73654d8fd5dfdb9
alekswaslet <1346150+alekswaslet@users.noreply.github.com>
2024-07-01 10:25:56 +0200

    Correctly use full nodeID value

--- src/transaction/types.ts
+++ src/transaction/types.ts
@@ -140,7 +140,7 @@ export class NodeId extends BytesData {
       throw new JuneoTypeError(`node id is not ${NodeIdSize} bytes long`)
     }
     super(buffer)
-    this.value = value
+    this.value = `NodeID-${parsedNodeId}`
   }
 }
 

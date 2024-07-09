b52217b31a80480b689389136eb4b7c18d621f5b
alekswaslet <1346150+alekswaslet@users.noreply.github.com>
2024-07-09 17:22:29 +0200

    Fix missing lint

--- tests/unit/transaction/input.test.ts
+++ tests/unit/transaction/input.test.ts
@@ -10,7 +10,6 @@ import {
 
 describe('Secp256k1Input', () => {
   test.each([
-
     {
       description: 'Amount min, address indices min',
       amount: BigInt(1),
@@ -71,14 +70,12 @@ describe('Secp256k1Input', () => {
       addressIndices: [],
       expectedValue: '00000005000000003b9aca0000000000'
     }
-
-  ])('$description',
-    ({ amount, addressIndices, expectedValue }) => {
-      const input = new Secp256k1Input(amount, addressIndices)
-      const serialized = input.serialize()
-      expect(serialized).toBeInstanceOf(JuneoBuffer)
-      expect(serialized.toHex()).toBe(expectedValue)
-    })
+  ])('$description', ({ amount, addressIndices, expectedValue }) => {
+    const input = new Secp256k1Input(amount, addressIndices)
+    const serialized = input.serialize()
+    expect(serialized).toBeInstanceOf(JuneoBuffer)
+    expect(serialized.toHex()).toBe(expectedValue)
+  })
 })
 
 describe('TransferableInput', () => {
@@ -89,7 +86,8 @@ describe('TransferableInput', () => {
       utxoIndex: 1,
       assetId: new AssetId('xNScMRJEmvx8A34Df2gQkTSjLMqWDcPbaPYfPYJRuf1cjLHLM'),
       input: new Secp256k1Input(BigInt(100), [1]),
-      expectedSerializedValue: '73ec83f2a46db234b02280c005ddcec7a961204c5f940ec4bf342c3a50693dec000000017db94035fa4902aee2c4c1e600d4e4d9cd9121ca57d23e98eeab45bdf88de8b50000000500000000000000640000000100000001'
+      expectedSerializedValue:
+        '73ec83f2a46db234b02280c005ddcec7a961204c5f940ec4bf342c3a50693dec000000017db94035fa4902aee2c4c1e600d4e4d9cd9121ca57d23e98eeab45bdf88de8b50000000500000000000000640000000100000001'
     },
     {
       description: 'Different UTXO index',
@@ -97,7 +95,8 @@ describe('TransferableInput', () => {
       utxoIndex: 2,
       assetId: new AssetId('xNScMRJEmvx8A34Df2gQkTSjLMqWDcPbaPYfPYJRuf1cjLHLM'),
       input: new Secp256k1Input(BigInt(200), [2]),
-      expectedSerializedValue: '636ded20c23c2b2bba3a6a46ace3811da6eda26c39fe8338f94c63c31e5b6962000000027db94035fa4902aee2c4c1e600d4e4d9cd9121ca57d23e98eeab45bdf88de8b50000000500000000000000c80000000100000002'
+      expectedSerializedValue:
+        '636ded20c23c2b2bba3a6a46ace3811da6eda26c39fe8338f94c63c31e5b6962000000027db94035fa4902aee2c4c1e600d4e4d9cd9121ca57d23e98eeab45bdf88de8b50000000500000000000000c80000000100000002'
     },
     {
       description: 'Multiple address indices',
@@ -105,7 +104,8 @@ describe('TransferableInput', () => {
       utxoIndex: 3,
       assetId: new AssetId('fawNQXm5Q8AzyvuvASN1NfvYuqqmvr55WQeD8ZibJz6Q12WP4'),
       input: new Secp256k1Input(BigInt(300), [1, 2]),
-      expectedSerializedValue: '750cb6424ff20ce3af51d1e79c4f70436de20cea2fffb8a370e1e191fdad04ce00000003579cbc918f783fe6a7fd30e0b7210ad7cbdbbd1808c2ba7c835a1a96cf5d7e1700000005000000000000012c000000020000000100000002'
+      expectedSerializedValue:
+        '750cb6424ff20ce3af51d1e79c4f70436de20cea2fffb8a370e1e191fdad04ce00000003579cbc918f783fe6a7fd30e0b7210ad7cbdbbd1808c2ba7c835a1a96cf5d7e1700000005000000000000012c000000020000000100000002'
     },
     {
       description: 'Zero UTXO index',
@@ -113,7 +113,8 @@ describe('TransferableInput', () => {
       utxoIndex: 0,
       assetId: new AssetId('2biwSVXZ8KMkwJbYnC4M6khf759pgr49ZPL4M1k8zvYjeQmG82'),
       input: new Secp256k1Input(BigInt(400), [3]),
-      expectedSerializedValue: 'e5b7f3ccdca9f8785bc2332964f2c8186ac22b98879397e32ff6cbc261d4aa5900000000d28a0223d17697e48048cc00004aabc6736b68f853b026c50a59f3132e418a050000000500000000000001900000000100000003'
+      expectedSerializedValue:
+        'e5b7f3ccdca9f8785bc2332964f2c8186ac22b98879397e32ff6cbc261d4aa5900000000d28a0223d17697e48048cc00004aabc6736b68f853b026c50a59f3132e418a050000000500000000000001900000000100000003'
     },
     {
       description: 'Empty address indices',
@@ -121,7 +122,8 @@ describe('TransferableInput', () => {
       utxoIndex: 4,
       assetId: new AssetId('2biwSVXZ8KMkwJbYnC4M6khf759pgr49ZPL4M1k8zvYjeQmG82'),
       input: new Secp256k1Input(BigInt(0), []),
-      expectedSerializedValue: '5989a9dcfbdd73148a327e867954a3876755f84a6c8049088dacc5d070eb40fb00000004d28a0223d17697e48048cc00004aabc6736b68f853b026c50a59f3132e418a0500000005000000000000000000000000'
+      expectedSerializedValue:
+        '5989a9dcfbdd73148a327e867954a3876755f84a6c8049088dacc5d070eb40fb00000004d28a0223d17697e48048cc00004aabc6736b68f853b026c50a59f3132e418a0500000005000000000000000000000000'
     },
     {
       description: 'High amount',
@@ -129,7 +131,8 @@ describe('TransferableInput', () => {
       utxoIndex: 6,
       assetId: new AssetId('2biwSVXZ8KMkwJbYnC4M6khf759pgr49ZPL4M1k8zvYjeQmG82'),
       input: new Secp256k1Input(BigInt('9007199254740991'), [5]),
-      expectedSerializedValue: '750cb6424ff20ce3af51d1e79c4f70436de20cea2fffb8a370e1e191fdad04ce00000006d28a0223d17697e48048cc00004aabc6736b68f853b026c50a59f3132e418a0500000005001fffffffffffff0000000100000005'
+      expectedSerializedValue:
+        '750cb6424ff20ce3af51d1e79c4f70436de20cea2fffb8a370e1e191fdad04ce00000006d28a0223d17697e48048cc00004aabc6736b68f853b026c50a59f3132e418a0500000005001fffffffffffff0000000100000005'
     },
     {
       description: 'Multiple address indices with high amount',
@@ -137,7 +140,8 @@ describe('TransferableInput', () => {
       utxoIndex: 7,
       assetId: new AssetId('XgN1Q8XeEuj9SU5UtM2LtNWsufgrz6amVs8BwGcJZM7ZuarNq'),
       input: new Secp256k1Input(BigInt('9007199254740991'), [5, 6]),
-      expectedSerializedValue: '5989a9dcfbdd73148a327e867954a3876755f84a6c8049088dacc5d070eb40fb0000000745a8de9ee8e1300fa74cd67b462c2eaf93faa7f415645ebaddfe936e5cf56f9300000005001fffffffffffff000000020000000500000006'
+      expectedSerializedValue:
+        '5989a9dcfbdd73148a327e867954a3876755f84a6c8049088dacc5d070eb40fb0000000745a8de9ee8e1300fa74cd67b462c2eaf93faa7f415645ebaddfe936e5cf56f9300000005001fffffffffffff000000020000000500000006'
     },
     {
       description: 'Zero amount with multiple address indices',
@@ -145,10 +149,16 @@ describe('TransferableInput', () => {
       utxoIndex: 8,
       assetId: new AssetId('XgN1Q8XeEuj9SU5UtM2LtNWsufgrz6amVs8BwGcJZM7ZuarNq'),
       input: new Secp256k1Input(BigInt(0), [7, 8]),
-      expectedSerializedValue: '6ed477de5bee3b3d5813b341e249b1b48747ef2c3bcce716cafeb2f43fb7031a0000000845a8de9ee8e1300fa74cd67b462c2eaf93faa7f415645ebaddfe936e5cf56f93000000050000000000000000000000020000000700000008'
+      expectedSerializedValue:
+        '6ed477de5bee3b3d5813b341e249b1b48747ef2c3bcce716cafeb2f43fb7031a0000000845a8de9ee8e1300fa74cd67b462c2eaf93faa7f415645ebaddfe936e5cf56f93000000050000000000000000000000020000000700000008'
     }
   ])('$description', ({ transactionId, utxoIndex, assetId, input, expectedSerializedValue }) => {
-    const transferableInput = new TransferableInput(transactionId, utxoIndex, assetId, input as TransactionInput & Serializable)
+    const transferableInput = new TransferableInput(
+      transactionId,
+      utxoIndex,
+      assetId,
+      input as TransactionInput & Serializable
+    )
     const serialized = transferableInput.serialize()
     expect(serialized).toBeInstanceOf(JuneoBuffer)
     expect(serialized.toHex()).toBe(expectedSerializedValue)
--- tests/unit/transaction/jevm/transaction.test.ts
+++ tests/unit/transaction/jevm/transaction.test.ts
@@ -1,9 +1,12 @@
 import {
-  Address, AssetId,
+  Address,
+  AssetId,
   BlockchainId,
-  EVMInput, EVMOutput,
+  EVMInput,
+  EVMOutput,
   JEVMExportTransaction,
-  JEVMImportTransaction, JuneoBuffer,
+  JEVMImportTransaction,
+  JuneoBuffer,
   Secp256k1Input,
   Secp256k1Output,
   TransactionId,
@@ -17,35 +20,40 @@ describe('EVMOutput', () => {
       address: new Address('0x3ffa70d2b129bca2cd2ed6a7027598aa46214997'),
       amount: BigInt(100),
       assetId: new AssetId('2biwSVXZ8KMkwJbYnC4M6khf759pgr49ZPL4M1k8zvYjeQmG82'),
-      expectedSerializedValue: '3ffa70d2b129bca2cd2ed6a7027598aa462149970000000000000064d28a0223d17697e48048cc00004aabc6736b68f853b026c50a59f3132e418a05'
+      expectedSerializedValue:
+        '3ffa70d2b129bca2cd2ed6a7027598aa462149970000000000000064d28a0223d17697e48048cc00004aabc6736b68f853b026c50a59f3132e418a05'
     },
     {
       description: 'Serialization with large amount',
       address: new Address('0x3ffa70d2b129bca2cd2ed6a7027598aa46214997'),
       amount: BigInt('9007199254740991'),
       assetId: new AssetId('2biwSVXZ8KMkwJbYnC4M6khf759pgr49ZPL4M1k8zvYjeQmG82'),
-      expectedSerializedValue: '3ffa70d2b129bca2cd2ed6a7027598aa46214997001fffffffffffffd28a0223d17697e48048cc00004aabc6736b68f853b026c50a59f3132e418a05'
+      expectedSerializedValue:
+        '3ffa70d2b129bca2cd2ed6a7027598aa46214997001fffffffffffffd28a0223d17697e48048cc00004aabc6736b68f853b026c50a59f3132e418a05'
     },
     {
       description: 'Serialization with different asset ID',
       address: new Address('0x3ffa70d2b129bca2cd2ed6a7027598aa46214997'),
       amount: BigInt(250),
       assetId: new AssetId('XgN1Q8XeEuj9SU5UtM2LtNWsufgrz6amVs8BwGcJZM7ZuarNq'),
-      expectedSerializedValue: '3ffa70d2b129bca2cd2ed6a7027598aa4621499700000000000000fa45a8de9ee8e1300fa74cd67b462c2eaf93faa7f415645ebaddfe936e5cf56f93'
+      expectedSerializedValue:
+        '3ffa70d2b129bca2cd2ed6a7027598aa4621499700000000000000fa45a8de9ee8e1300fa74cd67b462c2eaf93faa7f415645ebaddfe936e5cf56f93'
     },
     {
       description: 'Serialization with zero amount',
       address: new Address('0xb47e9dcd483278fdc19421858e7506947325614f'),
       amount: BigInt(0),
       assetId: new AssetId('2biwSVXZ8KMkwJbYnC4M6khf759pgr49ZPL4M1k8zvYjeQmG82'),
-      expectedSerializedValue: 'b47e9dcd483278fdc19421858e7506947325614f0000000000000000d28a0223d17697e48048cc00004aabc6736b68f853b026c50a59f3132e418a05'
+      expectedSerializedValue:
+        'b47e9dcd483278fdc19421858e7506947325614f0000000000000000d28a0223d17697e48048cc00004aabc6736b68f853b026c50a59f3132e418a05'
     },
     {
       description: 'Serialization with same address different asset',
       address: new Address('0xb47e9dcd483278fdc19421858e7506947325614f'),
       amount: BigInt(500),
       assetId: new AssetId('XgN1Q8XeEuj9SU5UtM2LtNWsufgrz6amVs8BwGcJZM7ZuarNq'),
-      expectedSerializedValue: 'b47e9dcd483278fdc19421858e7506947325614f00000000000001f445a8de9ee8e1300fa74cd67b462c2eaf93faa7f415645ebaddfe936e5cf56f93'
+      expectedSerializedValue:
+        'b47e9dcd483278fdc19421858e7506947325614f00000000000001f445a8de9ee8e1300fa74cd67b462c2eaf93faa7f415645ebaddfe936e5cf56f93'
     }
   ])('$description', ({ address, amount, assetId, expectedSerializedValue }) => {
     const evmOutput = new EVMOutput(address, amount, assetId)
@@ -63,7 +71,8 @@ describe('EVMInput', () => {
       amount: BigInt(200),
       assetId: new AssetId('XgN1Q8XeEuj9SU5UtM2LtNWsufgrz6amVs8BwGcJZM7ZuarNq'),
       nonce: BigInt(1),
-      expectedSerializedValue: 'b47e9dcd483278fdc19421858e7506947325614f00000000000000c845a8de9ee8e1300fa74cd67b462c2eaf93faa7f415645ebaddfe936e5cf56f930000000000000001'
+      expectedSerializedValue:
+        'b47e9dcd483278fdc19421858e7506947325614f00000000000000c845a8de9ee8e1300fa74cd67b462c2eaf93faa7f415645ebaddfe936e5cf56f930000000000000001'
     },
     {
       description: 'Serialization with extremely high amount',
@@ -71,7 +80,8 @@ describe('EVMInput', () => {
       amount: BigInt('9007199254740991'),
       assetId: new AssetId('tDxKdhyn2b9dNLMdsSv3xEY8ihGf7991XSWxXMzWu1bLtAued'),
       nonce: BigInt(10),
-      expectedSerializedValue: '23eb882f57a6e8173e2a6152883e1fb30c8926ec001fffffffffffff744f11ad6ec1e112762bbbba11f3f56db36bf3c0061020aec8d4332406d52df0000000000000000a'
+      expectedSerializedValue:
+        '23eb882f57a6e8173e2a6152883e1fb30c8926ec001fffffffffffff744f11ad6ec1e112762bbbba11f3f56db36bf3c0061020aec8d4332406d52df0000000000000000a'
     },
     {
       description: 'Serialization with very high nonce',
@@ -79,7 +89,8 @@ describe('EVMInput', () => {
       amount: BigInt(1000),
       assetId: new AssetId('G3mH67ubqNAJB6txHTHFtFzH56ynrhd2ynJrUk6RjT9iBzXbK'),
       nonce: BigInt('9007199254740991'),
-      expectedSerializedValue: 'a12a2ba31638bd51e105f882749546a206a70fc500000000000003e8222ae27e8c75732c641f35edc71b56b52179072b5c099317ad25ed2de5a93925001fffffffffffff'
+      expectedSerializedValue:
+        'a12a2ba31638bd51e105f882749546a206a70fc500000000000003e8222ae27e8c75732c641f35edc71b56b52179072b5c099317ad25ed2de5a93925001fffffffffffff'
     },
     {
       description: 'Serialization with zero values',
@@ -87,7 +98,8 @@ describe('EVMInput', () => {
       amount: BigInt(0),
       assetId: new AssetId('2REm6DRSgbVyE4dypnzBU9WWUV4zW9VcsTMHiDha7GLV84ZXCy'),
       nonce: BigInt(0),
-      expectedSerializedValue: '84a105dac8b065124c9bd3e1347c952c5748e5020000000000000000babacf29dfce5c3835259d6cdd5ea5c88bd1335f61f4baa7bc24cad03bcc43c60000000000000000'
+      expectedSerializedValue:
+        '84a105dac8b065124c9bd3e1347c952c5748e5020000000000000000babacf29dfce5c3835259d6cdd5ea5c88bd1335f61f4baa7bc24cad03bcc43c60000000000000000'
     }
   ])('$description', ({ address, amount, assetId, nonce, expectedSerializedValue }) => {
     const evmInput = new EVMInput(address, amount, assetId, nonce)
@@ -119,7 +131,8 @@ describe('JEVMImportTransaction', () => {
           new AssetId('2NQFaeBwMcACKqsKqMoLzYmVAHMYeFREfYJq6dtQnsJ5tTyk42')
         )
       ],
-      expectedSerializedValue: '00000000000000000001d28a0223d17697e48048cc00004aabc6736b68f853b026c50a59f3132e418a05d28a0223d17697e48048cc00004aabc6736b68f853b026c50a59f3132e418a05000000012589ad25990a9335e6c984490f2bf5056ba6e4a62c1a669d38f0c19974516a6f00000000b44a17bc82c895623d5fda84b07389ceb845424d8f6fba12b797eeb9368d37ab0000000500000000000003e80000000100000000000000019d865eebc7037472d780cdfb70ba2f0471998a320000000000000bb8b44a17bc82c895623d5fda84b07389ceb845424d8f6fba12b797eeb9368d37ab'
+      expectedSerializedValue:
+        '00000000000000000001d28a0223d17697e48048cc00004aabc6736b68f853b026c50a59f3132e418a05d28a0223d17697e48048cc00004aabc6736b68f853b026c50a59f3132e418a05000000012589ad25990a9335e6c984490f2bf5056ba6e4a62c1a669d38f0c19974516a6f00000000b44a17bc82c895623d5fda84b07389ceb845424d8f6fba12b797eeb9368d37ab0000000500000000000003e80000000100000000000000019d865eebc7037472d780cdfb70ba2f0471998a320000000000000bb8b44a17bc82c895623d5fda84b07389ceb845424d8f6fba12b797eeb9368d37ab'
     },
     {
       description: 'High values serialization for JEVMImportTransaction',
@@ -141,7 +154,8 @@ describe('JEVMImportTransaction', () => {
           new AssetId('2NQFaeBwMcACKqsKqMoLzYmVAHMYeFREfYJq6dtQnsJ5tTyk42')
         )
       ],
-      expectedSerializedValue: '00000000000000000002d28a0223d17697e48048cc00004aabc6736b68f853b026c50a59f3132e418a05d28a0223d17697e48048cc00004aabc6736b68f853b026c50a59f3132e418a05000000012589ad25990a9335e6c984490f2bf5056ba6e4a62c1a669d38f0c19974516a6f00000001b44a17bc82c895623d5fda84b07389ceb845424d8f6fba12b797eeb9368d37ab00000005001fffffffffffff0000000100000001000000019d865eebc7037472d780cdfb70ba2f0471998a32001fffffffffffffb44a17bc82c895623d5fda84b07389ceb845424d8f6fba12b797eeb9368d37ab'
+      expectedSerializedValue:
+        '00000000000000000002d28a0223d17697e48048cc00004aabc6736b68f853b026c50a59f3132e418a05d28a0223d17697e48048cc00004aabc6736b68f853b026c50a59f3132e418a05000000012589ad25990a9335e6c984490f2bf5056ba6e4a62c1a669d38f0c19974516a6f00000001b44a17bc82c895623d5fda84b07389ceb845424d8f6fba12b797eeb9368d37ab00000005001fffffffffffff0000000100000001000000019d865eebc7037472d780cdfb70ba2f0471998a32001fffffffffffffb44a17bc82c895623d5fda84b07389ceb845424d8f6fba12b797eeb9368d37ab'
     },
 
     {
@@ -164,17 +178,10 @@ describe('JEVMImportTransaction', () => {
           new AssetId('2NQFaeBwMcACKqsKqMoLzYmVAHMYeFREfYJq6dtQnsJ5tTyk42')
         )
       ],
-      expectedSerializedValue: '00000000000000000003d28a0223d17697e48048cc00004aabc6736b68f853b026c50a59f3132e418a05d28a0223d17697e48048cc00004aabc6736b68f853b026c50a59f3132e418a05000000012589ad25990a9335e6c984490f2bf5056ba6e4a62c1a669d38f0c19974516a6f00000000b44a17bc82c895623d5fda84b07389ceb845424d8f6fba12b797eeb9368d37ab0000000500000000000000000000000100000000000000019d865eebc7037472d780cdfb70ba2f0471998a320000000000000000b44a17bc82c895623d5fda84b07389ceb845424d8f6fba12b797eeb9368d37ab'
+      expectedSerializedValue:
+        '00000000000000000003d28a0223d17697e48048cc00004aabc6736b68f853b026c50a59f3132e418a05d28a0223d17697e48048cc00004aabc6736b68f853b026c50a59f3132e418a05000000012589ad25990a9335e6c984490f2bf5056ba6e4a62c1a669d38f0c19974516a6f00000000b44a17bc82c895623d5fda84b07389ceb845424d8f6fba12b797eeb9368d37ab0000000500000000000000000000000100000000000000019d865eebc7037472d780cdfb70ba2f0471998a320000000000000000b44a17bc82c895623d5fda84b07389ceb845424d8f6fba12b797eeb9368d37ab'
     }
-
-  ])('$description', ({
-    networkId,
-    blockchainId,
-    sourceChain,
-    importedInputs,
-    outputs,
-    expectedSerializedValue
-  }) => {
+  ])('$description', ({ networkId, blockchainId, sourceChain, importedInputs, outputs, expectedSerializedValue }) => {
     const jevmImportTransaction = new JEVMImportTransaction(
       networkId,
       blockchainId,
@@ -206,18 +213,11 @@ describe('JEVMExportTransaction', () => {
       exportedOutputs: [
         new TransferableOutput(
           new AssetId('25revVW7o2DgkhQPTkbGLNxscS6G9Mj6eTu7PCgKvzw3HM7pJv'),
-          new Secp256k1Output(
-            BigInt(1000),
-            BigInt(0),
-            1,
-            [
-              new Address('0x0123456789abcdef0123456789abcdef01234567')
-
-            ]
-          )
+          new Secp256k1Output(BigInt(1000), BigInt(0), 1, [new Address('0x0123456789abcdef0123456789abcdef01234567')])
         )
       ],
-      expectedSerializedValue: '00000000000100000001d33edb0dcd6df2b65e889ceb734da2041f1078512ac60de6aa87579f77b4c0e1d33edb0dcd6df2b65e889ceb734da2041f1078512ac60de6aa87579f77b4c0e1000000010123456789abcdef0123456789abcdef0123456700000000000003e88eb8f382e28b56222a34df0cf68fefab22cec085d2fc7d047561210cb7c3ef470000000000000001000000018eb8f382e28b56222a34df0cf68fefab22cec085d2fc7d047561210cb7c3ef470000000700000000000003e8000000000000000000000001000000010123456789abcdef0123456789abcdef01234567'
+      expectedSerializedValue:
+        '00000000000100000001d33edb0dcd6df2b65e889ceb734da2041f1078512ac60de6aa87579f77b4c0e1d33edb0dcd6df2b65e889ceb734da2041f1078512ac60de6aa87579f77b4c0e1000000010123456789abcdef0123456789abcdef0123456700000000000003e88eb8f382e28b56222a34df0cf68fefab22cec085d2fc7d047561210cb7c3ef470000000000000001000000018eb8f382e28b56222a34df0cf68fefab22cec085d2fc7d047561210cb7c3ef470000000700000000000003e8000000000000000000000001000000010123456789abcdef0123456789abcdef01234567'
     },
     {
       description: 'JEVMExportTransaction with multiple inputs and outputs',
@@ -241,17 +241,11 @@ describe('JEVMExportTransaction', () => {
       exportedOutputs: [
         new TransferableOutput(
           new AssetId('25revVW7o2DgkhQPTkbGLNxscS6G9Mj6eTu7PCgKvzw3HM7pJv'),
-          new Secp256k1Output(
-            BigInt(2000),
-            BigInt(0),
-            1,
-            [
-              new Address('0xfedcba9876543210fedcba9876543210fedcba98')
-            ]
-          )
+          new Secp256k1Output(BigInt(2000), BigInt(0), 1, [new Address('0xfedcba9876543210fedcba9876543210fedcba98')])
         )
       ],
-      expectedSerializedValue: '00000000000100000002d33edb0dcd6df2b65e889ceb734da2041f1078512ac60de6aa87579f77b4c0e1d33edb0dcd6df2b65e889ceb734da2041f1078512ac60de6aa87579f77b4c0e1000000020123456789abcdef0123456789abcdef0123456700000000000001f48eb8f382e28b56222a34df0cf68fefab22cec085d2fc7d047561210cb7c3ef47000000000000000289abcdef0123456789abcdef0123456789abcdef00000000000005dc8eb8f382e28b56222a34df0cf68fefab22cec085d2fc7d047561210cb7c3ef470000000000000003000000018eb8f382e28b56222a34df0cf68fefab22cec085d2fc7d047561210cb7c3ef470000000700000000000007d000000000000000000000000100000001fedcba9876543210fedcba9876543210fedcba98'
+      expectedSerializedValue:
+        '00000000000100000002d33edb0dcd6df2b65e889ceb734da2041f1078512ac60de6aa87579f77b4c0e1d33edb0dcd6df2b65e889ceb734da2041f1078512ac60de6aa87579f77b4c0e1000000020123456789abcdef0123456789abcdef0123456700000000000001f48eb8f382e28b56222a34df0cf68fefab22cec085d2fc7d047561210cb7c3ef47000000000000000289abcdef0123456789abcdef0123456789abcdef00000000000005dc8eb8f382e28b56222a34df0cf68fefab22cec085d2fc7d047561210cb7c3ef470000000000000003000000018eb8f382e28b56222a34df0cf68fefab22cec085d2fc7d047561210cb7c3ef470000000700000000000007d000000000000000000000000100000001fedcba9876543210fedcba9876543210fedcba98'
     },
     {
       description: 'JEVMExportTransaction with higher nonce and multiple addresses',
@@ -269,36 +263,28 @@ describe('JEVMExportTransaction', () => {
       exportedOutputs: [
         new TransferableOutput(
           new AssetId('25revVW7o2DgkhQPTkbGLNxscS6G9Mj6eTu7PCgKvzw3HM7pJv'),
-          new Secp256k1Output(
-            BigInt(3000),
-            BigInt(0),
-            2,
-            [
-              new Address('0x0123456789abcdef0123456789abcdef01234567'),
-              new Address('0x89abcdef0123456789abcdef0123456789abcdef')
-            ]
-          )
+          new Secp256k1Output(BigInt(3000), BigInt(0), 2, [
+            new Address('0x0123456789abcdef0123456789abcdef01234567'),
+            new Address('0x89abcdef0123456789abcdef0123456789abcdef')
+          ])
         )
       ],
-      expectedSerializedValue: '00000000000100000001d33edb0dcd6df2b65e889ceb734da2041f1078512ac60de6aa87579f77b4c0e1d33edb0dcd6df2b65e889ceb734da2041f1078512ac60de6aa87579f77b4c0e100000001abcdef0123456789abcdef0123456789abcdef010000000000000bb88eb8f382e28b56222a34df0cf68fefab22cec085d2fc7d047561210cb7c3ef47000000000000000a000000018eb8f382e28b56222a34df0cf68fefab22cec085d2fc7d047561210cb7c3ef47000000070000000000000bb8000000000000000000000002000000020123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef'
+      expectedSerializedValue:
+        '00000000000100000001d33edb0dcd6df2b65e889ceb734da2041f1078512ac60de6aa87579f77b4c0e1d33edb0dcd6df2b65e889ceb734da2041f1078512ac60de6aa87579f77b4c0e100000001abcdef0123456789abcdef0123456789abcdef010000000000000bb88eb8f382e28b56222a34df0cf68fefab22cec085d2fc7d047561210cb7c3ef47000000000000000a000000018eb8f382e28b56222a34df0cf68fefab22cec085d2fc7d047561210cb7c3ef47000000070000000000000bb8000000000000000000000002000000020123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef'
     }
-  ])('$description', ({
-    networkId,
-    blockchainId,
-    destinationChain,
-    inputs,
-    exportedOutputs,
-    expectedSerializedValue
-  }) => {
-    const jevmExportTransaction = new JEVMExportTransaction(
-      networkId,
-      blockchainId,
-      destinationChain,
-      inputs,
-      exportedOutputs
-    )
-    const serialized = jevmExportTransaction.serialize()
-    expect(serialized).toBeInstanceOf(JuneoBuffer)
-    expect(serialized.toHex()).toBe(expectedSerializedValue)
-  })
+  ])(
+    '$description',
+    ({ networkId, blockchainId, destinationChain, inputs, exportedOutputs, expectedSerializedValue }) => {
+      const jevmExportTransaction = new JEVMExportTransaction(
+        networkId,
+        blockchainId,
+        destinationChain,
+        inputs,
+        exportedOutputs
+      )
+      const serialized = jevmExportTransaction.serialize()
+      expect(serialized).toBeInstanceOf(JuneoBuffer)
+      expect(serialized.toHex()).toBe(expectedSerializedValue)
+    }
+  )
 })

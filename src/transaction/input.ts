f19cc954a47683a03a798ae1b0f0d2dc234ed0ef
alekswaslet <1346150+alekswaslet@users.noreply.github.com>
2024-07-14 12:19:12 +0200

    Added StakeableLockedInput

--- src/transaction/input.ts
+++ src/transaction/input.ts
@@ -1,6 +1,6 @@
 import { type Blockchain } from '../chain'
 import { InputError, JuneoBuffer, ParsingError, type Serializable, SignatureError } from '../utils'
-import { AssetIdSize, Secp256k1InputTypeId, TransactionIdSize } from './constants'
+import { AssetIdSize, Secp256k1InputTypeId, StakeableLockedInputTypeId, TransactionIdSize } from './constants'
 import { type Utxo } from './output'
 import { AbstractSignable } from './signature'
 import { type Address, AssetId, TransactionId } from './types'
@@ -168,5 +168,43 @@ export class Secp256k1Input implements TransactionInput {
       indices.push(indice)
     }
     return new Secp256k1Input(amount, indices)
+  }
+}
+
+export class StakeableLockedInput implements TransactionInput {
+  utxo: Utxo | undefined
+  readonly typeId = StakeableLockedInputTypeId
+  locktime: bigint
+  amount: bigint
+  addressIndices: number[]
+
+  constructor (locktime: bigint, amount: bigint, addressIndices: number[], utxo?: Utxo) {
+    this.utxo = utxo
+    this.locktime = locktime
+    this.amount = amount
+    this.addressIndices = addressIndices
+    this.addressIndices.sort((a: number, b: number) => {
+      return a - b
+    })
+  }
+
+  serialize (): JuneoBuffer {
+    const buffer = JuneoBuffer.alloc(
+      // 4 + 8 + 4 + 8 + 4 = 28
+      28 + 4 * this.addressIndices.length
+    )
+    buffer.writeUInt32(this.typeId)
+    buffer.writeUInt64(this.locktime)
+    const transferableInput = new Secp256k1Input(this.amount, this.addressIndices, this.utxo)
+    buffer.write(transferableInput)
+    return buffer
+  }
+
+  static parse (data: string | JuneoBuffer): StakeableLockedInput {
+    const reader = JuneoBuffer.from(data).createReader()
+    reader.readAndVerifyTypeId(StakeableLockedInputTypeId)
+    const locktime = reader.readUInt64()
+    const input = Secp256k1Input.parse(reader.readRemaining())
+    return new StakeableLockedInput(locktime, input.amount, input.addressIndices, input.utxo)
   }
 }
